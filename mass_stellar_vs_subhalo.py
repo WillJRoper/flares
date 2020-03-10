a14d0bf5d23bd0a8dd7b3097a32f000acc883b6c
Will Roper <wjr21@sussex.ac.uk>
2020-03-10 23:33:09 +0000

    updated mass plot script

--- mass_stellar_vs_subhalo.py
+++ mass_stellar_vs_subhalo.py
@@ -20,37 +20,51 @@ for reg in range(0, 1):
 snap = '010_z005p000'
 
 submass_dict = {}
-starmass_dict = {}
 for reg in regions:
 
     print(reg)
 
     path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
 
-    starmass_dict[reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/ApertureMeasurements/Mass/010kpc', noH=True)[:, 4]
     submass_dict[reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/Mass', noH=True)
 
 submass = np.concatenate(list(submass_dict.values())) * 10**10
-starmass = np.concatenate(list(starmass_dict.values())) * 10**10
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
 
-# submass = submass[starmass > 0]
-# starmass = starmass[starmass > 0]
-# starmass = starmass[submass > 0]
-# submass = submass[submass > 0]
+starmass_dict = {}
+for part, ax in zip([0, 4, 5], [ax1, ax2, ax3]):
 
-cbar = ax.hexbin(submass+1, starmass+1, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
-                 yscale='log', linewidths=0.2, cmap='viridis')
+    for reg in regions:
 
-ax.plot(np.linspace(submass.min(), submass.max(), 100), np.linspace(submass.min(), submass.max(), 100), linestyle='--')
+        print(reg)
 
-ax.set_xlabel('$M_{\mathrm{sub}}/M_\odot+1$')
-ax.set_ylabel('$M_{*}/M_\odot+1$')
+        path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
 
-cax = fig.colorbar(cbar, ax=ax)
-cax.ax.set_ylabel(r'$N$')
+        starmass_dict[reg] = E.read_array('SUBFIND', path, snap,
+                                          'Subhalo/ApertureMeasurements/Mass/010kpc', noH=True)[:, part]
+
+    starmass = np.concatenate(list(starmass_dict.values())) * 10**10
+
+    # submass = submass[starmass > 0]
+    # starmass = starmass[starmass > 0]
+    # starmass = starmass[submass > 0]
+    # submass = submass[submass > 0]
+
+    cbar = ax.hexbin(submass+1, starmass+1, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
+                     yscale='log', linewidths=0.2, cmap='viridis')
+
+    ax.plot(np.linspace(submass.min(), submass.max(), 100), np.linspace(submass.min(), submass.max(), 100),
+            linestyle='--')
+
+    ax.set_xlabel('$M_{\mathrm{sub}}/M_\odot+1$')
+    ax.set_ylabel('$M_{*}/M_\odot+1$')
+
+    cax = fig.colorbar(cbar, ax=ax)
+    cax.ax.set_ylabel(r'$N$')
 
 fig.savefig('plots/starvssubhalo_mass_' + snap + '.png', bbox_inches='tight')
 
