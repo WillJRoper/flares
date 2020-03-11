8c38b74982e7c9289824362461c4a409c9ecc334
Will Roper <wjr21@sussex.ac.uk>
2020-03-11 12:49:33 +0000

    added zero mass probe script

--- zeros_probe.py
+++ zeros_probe.py
@@ -44,14 +44,14 @@ ax3 = fig.add_subplot(143)
 
 starmass_dict = {}
 cbars = {}
-labels = ['$M_{\mathrm{gas}}/M_\odot + 1$', '$M_{\mathrm{DM}}/M_\odot + 1$', '$M_{*}/M_\odot + 1$']
+labels = ['$M_{\mathrm{gas}}/M_\odot + 1$', '$M_{\mathrm{DM}}/M_\odot + 1$', '_', '_', '$M_{*}/M_\odot + 1$']
 for drop, parts, ax, title in zip([0, 1, 4], [(1, 4), (0, 4), (0, 1)], [ax1, ax2, ax3],
                                   ['$M_{\mathrm{gas}}=0$', '$M_{\mathrm{DM}}=0$', '$M_{*}=0$', 'BH']):
 
     x_plt = starmass[:, parts[0]][np.where(starmass[:, drop] == 0)] + 1
     y_plt = starmass[:, parts[1]][np.where(starmass[:, drop] == 0)] + 1
 
-    cbars[drop] = ax.hexbin(x_plt, y_plt, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
+    cbars[drop] = ax.hexbin(x_plt, y_plt, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(1, 10**6),
                             yscale='log', linewidths=0.2, cmap='viridis', zorder=1)
 
     ax.set_xscale('log')
@@ -59,7 +59,8 @@ for drop, parts, ax, title in zip([0, 1, 4], [(1, 4), (0, 4), (0, 1)], [ax1, ax2
 
     ax.set_title(title)
 
-    # ax.set_ylim(10**6, 10**13)
+    ax.set_xlim(0.5 * 10**-1, None)
+    ax.set_ylim(0.5 * 10 ** -1, None)
 
     ax.set_xlabel(labels[parts[0]])
     ax.set_ylabel(labels[parts[1]])
