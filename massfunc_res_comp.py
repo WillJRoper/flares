86493f2a5ca771b97dee9508442e64ad030a7b6a
Will Roper <wjr21@sussex.ac.uk>
2020-03-11 09:31:49 +0000

    edited rescomp mass functions

--- massfunc_res_comp.py
+++ massfunc_res_comp.py
@@ -29,17 +29,21 @@ def get_mass_data(path, snap, tag, group="SUBFIND_GROUP", noH=True, cut_bounds=T
 
     return M_dat
 
+tag = "FOF/Group_M_Crit200"
+snap = '010_z0065000'
+group = "SUBFIND_GROUP"
+
 # Extarct M_200s
-M_200 = get_mass_data('/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_0032/data/', '009_z006p000',
-                      "FOF/Group_M_Crit200", group="SUBFIND_GROUP", noH=True, cut_bounds=True)
-M_200_hrDMO = get_mass_data('/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_0032_hires/data/', '009_z006p000',
-                      "FOF/Group_M_Crit200", group="SUBFIND_GROUP", noH=True, cut_bounds=True)
-M_200_hr = get_mass_data('/cosma7/data/dp004/dc-rope1/G-EAGLE32_hires/data/', '009_z006p000',
-                      "FOF/Group_M_Crit200", group="SUBFIND_GROUP", noH=True, cut_bounds=True)
+M_200 = get_mass_data('/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_0038/data/', snap,
+                      tag, group=group, noH=True, cut_bounds=True)
+# M_200_hrDMO = get_mass_data('/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_0032_hires/data/', snap,
+#                       "FOF/Group_M_Crit200", group=group, noH=True, cut_bounds=True)
+M_200_hr = get_mass_data('/cosma/home/dp004/dc-rope1/FLARES/FLARES-HD/FLARES_HR_EMOSAICS_38/data/', snap,
+                         tag, group=group, noH=True, cut_bounds=True)
 
 M_200 = M_200[np.where(M_200 != 0.0)] * 10**10
 M_200_hr = M_200_hr[np.where(M_200_hr != 0.0)] * 10**10
-M_200_hrDMO = M_200_hrDMO[np.where(M_200_hrDMO != 0.0)] * 10**10
+# M_200_hrDMO = M_200_hrDMO[np.where(M_200_hrDMO != 0.0)] * 10**10
 
 print('Minimums:', M_200.min(), M_200_hr.min())
 print('Maximums:', M_200.max(), M_200_hr.max())
@@ -57,7 +61,7 @@ interval = bins[1:] - bins[:-1]
 # Histogram the DMLJ halo masses
 H, bins = np.histogram(M_200, bins=bins)
 H_hr, _ = np.histogram(M_200_hr, bins=bins)
-H_hrDMO, _ = np.histogram(M_200_hrDMO, bins=bins)
+# H_hrDMO, _ = np.histogram(M_200_hrDMO, bins=bins)
 
 # Remove zeros for plotting
 #H = H[np.where(H != 0)]
@@ -73,7 +77,7 @@ bin_cents = bins[1:] - ((bins[1] - bins[0]) / 2)
 # Plot each histogram
 ax.loglog(bin_cents, H/interval, label='"Standard" Resolution')
 ax.loglog(bin_cents, H_hr/interval, linestyle='--', label='High Resolution')
-ax.loglog(bin_cents, H_hrDMO/interval, linestyle='--', label='High Resolution (DMO)')
+# ax.loglog(bin_cents, H_hrDMO/interval, linestyle='--', label='High Resolution (DMO)')
 
 # Label axes
 ax.set_xlabel(r'$M_{200}/M_\odot$')
@@ -84,4 +88,4 @@ handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles, labels)
 
 # Save figure
-fig.savefig('mass_func_res_comp.png', bbox_inches='tight')
+fig.savefig('mass_func_res_comp' + snap + '.png', bbox_inches='tight')
