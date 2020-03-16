078ea5241c0f1f7056b6576ecc52c5db547d3bff
Will Roper <wjr21@sussex.ac.uk>
2020-03-16 16:54:35 +0000

    Added all redshift half mass rad script

---
+++ SFH_allgals.py
@@ -0,0 +1,138 @@
+#!/cosma/home/dp004/dc-rope1/.conda/envs/flares-env/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import astropy.units as u
+import astropy.constants as cons
+from astropy.cosmology import Planck13 as cosmo
+from matplotlib.colors import LogNorm
+import eagle_IO as E
+import seaborn as sns
+from flares import flares
+matplotlib.use('Agg')
+
+sns.set_style('whitegrid')
+
+
+def calc_srf(z, a_born, mass, t_bin=100):
+
+    # Convert scale factor into redshift
+    z_born = 1 / a_born - 1
+
+    # Convert to time in Gyrs
+    t = cosmo.age(z)
+    t_born = cosmo.age(z_born)
+
+    # Calculate the VR
+    age = (t - t_born).to(u.Myr)
+
+    ok = np.where(age.value <= t_bin)[0]
+    if len(ok) > 0:
+
+        # Calculate the SFR
+        sfr = np.sum(mass[ok]) / (t_bin * 1e6)
+
+    else:
+        sfr = 0.0
+
+    return sfr
+
+
+regions = []
+for reg in range(0, 40):
+
+    if reg < 10:
+        regions.append('000' + str(reg))
+    else:
+        regions.append('00' + str(reg))
+
+snaps = ['000_z015p000', '001_z014p000', '002_z013p000', '003_z012p000', '004_z011p000', '005_z010p000',
+             '006_z009p000', '007_z008p000', '008_z007p000', '009_z006p000', '010_z005p000', '011_z004p770']
+
+zs_dict = {}
+stellar_a_dict = {}
+starmass_dict = {}
+halo_id_part_inds = {}
+for snap in snaps:
+
+    stellar_a_dict[snap] = {}
+    starmass_dict[snap] = {}
+    halo_id_part_inds[snap] = {}
+
+for reg in regions:
+
+    for snap in snaps:
+
+        print(reg, snap)
+
+        path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
+        try:
+            starmass_dict[snap][reg] = E.read_array('SNAP', path, snap, 'PartType4/Mass',
+                                                    noH=True, numThreads=8) * 10**10
+            stellar_a_dict[snap][reg] = E.read_array('SNAP', path, snap, 'PartType4/StellarFormationTime',
+                                                      noH=True, numThreads=8)
+            group_ids = E.read_array('PARTDATA', path, snap, 'PartType4/SubGroupNumber', numThreads=8)
+        except:
+            continue
+
+        # Get IDs of each subhalo
+        halo_id_part_inds[snap][reg] = {}
+        for pid, simid in enumerate(group_ids):
+            simid = int(simid)
+            if simid == 2**30:
+                continue
+            halo_id_part_inds[snap][reg].setdefault(simid, set()).update({pid})
+
+sfrs_gals = {}
+for snap in halo_id_part_inds.keys():
+    sfrs_gals[snap] = {}
+    z_str = snap.split('z')[1].split('p')
+    z = float(z_str[0] + '.' + z_str[1])
+    for reg in halo_id_part_inds[snap].keys():
+        sfrs_gals[snap][reg] = {}
+        for grp in halo_id_part_inds[snap][reg].keys():
+            parts = list(halo_id_part_inds[snap][reg][grp])
+            sfrs_gals[snap][reg][grp] = calc_srf(z, stellar_a_dict[snap][reg][parts], starmass_dict[snap][reg][parts])
+zs = {}
+zs_plt = []
+sfrs = {}
+for snap in snaps:
+
+    print(snap)
+    z_str = snap.split('z')[1].split('p')
+    z = float(z_str[0] + '.' + z_str[1])
+    # zs[snap] = np.full_like(starmass[snap], z)
+    zs_plt.append(z)
+    sfrs[snap] = np.concatenate([list(sfrs_gals[snap][reg].values()) for reg in sfrs_gals[snap].keys()])
+
+# hex_sfrs = np.concatenate(list(sfrs.values()))
+# hex_zs = np.concatenate(list(zs.values()))
+
+medians = np.zeros(len(snaps))
+pcent84 = np.zeros(len(snaps))
+pcent16 = np.zeros(len(snaps))
+for ind, snap in enumerate(snaps):
+
+    print(ind, snap)
+
+    medians[ind] = np.median(sfrs[snap])
+    pcent84[ind] = np.percentile(sfrs[snap], 84)
+    pcent16[ind] = np.percentile(sfrs[snap], 16)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+# cbar = ax.hexbin(hex_zs, hex_sfrs, gridsize=100, mincnt=1, norm=LogNorm(), yscale='log',
+#                  linewidths=0.2, cmap='Greys', zorder=0)
+ax.plot(zs_plt, medians, linestyle='--', color='r')
+ax.fill_between(zs_plt, pcent16, pcent84, alpha=0.4, color='g')
+
+ax.set_xlabel('$z$')
+ax.set_ylabel('SFR / $[M_\odot/\mathrm{yr}]$')
+
+ax.set_yscale('log')
+
+# cax = fig.colorbar(cbar, ax=ax)
+# cax.ax.set_ylabel(r'$N$')
+
+fig.savefig('plots/SFH.png')
---
+++ subhalf_mass_scatter_allzs.py
@@ -0,0 +1,98 @@
+#!/cosma/home/dp004/dc-rope1/.conda/envs/flares-env/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import astropy.units as u
+from matplotlib.colors import LogNorm
+import matplotlib.gridspec as gridspec
+import eagle_IO as E
+import seaborn as sns
+import pickle
+import itertools
+matplotlib.use('Agg')
+
+sns.set_style('whitegrid')
+
+
+regions = []
+for reg in range(0, 1):
+
+    if reg < 10:
+        regions.append('000' + str(reg))
+    else:
+        regions.append('00' + str(reg))
+
+snaps = ['005_z010p000', '006_z009p000', '007_z008p000', '008_z007p000', '009_z006p000', '010_z005p000']
+axlims_x = []
+axlims_y = []
+
+half_mass_rads_dict = {}
+xaxis_dict = {}
+for snap in snaps:
+
+    half_mass_rads_dict[snap] = {}
+    xaxis_dict[snap] = {}
+
+for reg in regions:
+
+    for snap in snaps:
+
+        print(reg)
+
+        path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
+
+        half_mass_rads_dict[snap][reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/HalfMassRad', noH=True)[:, 4]
+        xaxis_dict[snap][reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+                                             noH=True, numThreads=8)[:, 4]
+
+# Set up plot
+fig = plt.figure(figsize=(18, 6))
+gs = gridspec.GridSpec(2, 6)
+gs.update(wspace=0.0, hspace=0.0)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[0, 1])
+ax3 = fig.add_subplot(gs[0, 2])
+ax4 = fig.add_subplot(gs[1, 0])
+ax5 = fig.add_subplot(gs[1, 1])
+ax6 = fig.add_subplot(gs[1, 2])
+
+for ax, snap, (i, j) in zip([ax1, ax2, ax3, ax4, ax5, ax6], snaps, [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]):
+
+    z_str = snap.split('z')[1].split('p')
+    z = float(z_str[0] + '.' + z_str[1])
+
+    xs = np.concatenate(list(xaxis_dict[snap].values()))
+    half_mass_rads_plt = np.concatenate(list(half_mass_rads_dict[snap].values()))
+    cbar = ax.hexbin(xs, half_mass_rads_plt, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
+                     linewidths=0.2, cmap='viridis')
+
+    ax.text(0.8, 0.9, f'$z={z}$', bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
+            transform=ax.transAxes, horizontalalignment='right', fontsize=8)
+
+    axlims_x.extend(ax.get_xlim())
+    axlims_y.extend(ax.get_ylim())
+
+    # Label axes
+    if i == 1:
+        ax.set_xlabel(r'$M_{\star}/M_\odot$')
+    if j == 0:
+        ax.set_ylabel('$R_{1/2}/$ckpc')
+
+for ax in [ax1, ax2, ax3, ax4, ax5, ax6]:
+
+    ax.set_xlim(np.min(axlims_x), np.max(axlims_x))
+    ax.set_ylim(np.min(axlims_y), np.max(axlims_y))
+
+# Remove axis labels
+ax1.tick_params(axis='x', top=False, bottom=False, labeltop=False, labelbottom=False)
+ax2.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False,
+                labelright=False, labelbottom=False)
+ax3.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False,
+                labelright=False, labelbottom=False)
+ax5.tick_params(axis='y', left=False, right=False, labelleft=False, labelright=False)
+ax6.tick_params(axis='y', left=False, right=False, labelleft=False, labelright=False)
+
+fig.savefig('plots/HalfMassRadius_all_snaps.png',
+            bbox_inches='tight')
+
+plt.close(fig)
