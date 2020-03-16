bf2d7359041e0c1babf85e722859e5cbef061ad1
Will Roper <wjr21@sussex.ac.uk>
2020-03-16 18:14:28 +0000

    Added all redshift half mass rad script

--- subhalf_mass_scatter_allzs.py
+++ subhalf_mass_scatter_allzs.py
@@ -44,7 +44,7 @@ for reg in regions:
         path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
 
         half_mass_rads_dict[snap][reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/HalfMassRad', noH=True,
-                                                      numThreads=8)[:, 4]
+                                                      numThreads=8)[:, 4] * 1e3
         xaxis_dict[snap][reg] = E.read_array('SUBFIND', path, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
                                              noH=True, numThreads=8)[:, 4] * 10**10
 
