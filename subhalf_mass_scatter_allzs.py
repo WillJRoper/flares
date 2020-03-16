5bf4243797a1e46390a3a5e565da4936768f8d8e
Will Roper <wjr21@sussex.ac.uk>
2020-03-16 17:02:57 +0000

    Added all redshift half mass rad script

--- subhalf_mass_scatter_allzs.py
+++ subhalf_mass_scatter_allzs.py
@@ -37,7 +37,7 @@ for reg in regions:
 
     for snap in snaps:
 
-        print(reg)
+        print(reg, snap)
 
         path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
 
@@ -63,7 +63,13 @@ for ax, snap, (i, j) in zip([ax1, ax2, ax3, ax4, ax5, ax6], snaps, [(0, 0), (0, 
 
     xs = np.concatenate(list(xaxis_dict[snap].values()))
     half_mass_rads_plt = np.concatenate(list(half_mass_rads_dict[snap].values()))
-    cbar = ax.hexbin(xs, half_mass_rads_plt, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
+    
+    xs_plt = xs[half_mass_rads_plt > 0]
+    half_mass_rads_plt_plt = half_mass_rads_plt[half_mass_rads_plt > 0]
+    half_mass_rads_plt_plt = half_mass_rads_plt_plt[xs_plt > 0]
+    xs_plt = xs_plt[xs_plt > 0]
+    
+    cbar = ax.hexbin(xs_plt, half_mass_rads_plt, gridsize=100, mincnt=1, xscale='log', norm=LogNorm(),
                      linewidths=0.2, cmap='viridis')
 
     ax.text(0.8, 0.9, f'$z={z}$', bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
