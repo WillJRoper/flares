6b80e1a93df0c3d9c97521eda8c6bfafcbe38376
Will Roper <wjr21@sussex.ac.uk>
2020-03-17 15:27:43 +0000

    Fixed bug with subgroup IDs

--- subhalf_mass_scatter_threshgals.py
+++ subhalf_mass_scatter_threshgals.py
@@ -104,7 +104,8 @@ for reg, greg in zip(regions, gregions):
 
             hdf = h5py.File('/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/MergerGraphs/GEAGLE_' + greg +
                             '/SubMgraph_' + snap + '_PartType1.hdf5', 'r')
-            progs = np.concatenate([hdf[str(halo)]['Prog_haloIDs'][...] for halo in halos])
+            progs = np.concatenate([hdf[str(halo)]['Prog_haloIDs'][...]
+                                    for halo in halos if str(halo).split('.')[1] != 0])
             halos = list(set(progs))
             hdf.close()
 
