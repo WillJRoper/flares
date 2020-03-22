9bda73d503664718f5624dfa70923a0f73cabd02
Will Roper <wjr21@sussex.ac.uk>
2020-03-22 01:10:42 +0000

    UV simulation resolution image script added

--- UV_images.py
+++ UV_images.py
@@ -345,7 +345,7 @@ snaps = ['003_z012p000', '004_z011p000', '005_z010p000',
          '009_z006p000', '010_z005p000', '011_z004p770']
 
 reg_snaps = []
-for reg in regions:
+for reg in reversed(regions):
 
     for snap in snaps:
 
