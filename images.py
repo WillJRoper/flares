ca131a5c054dce570afedfcec034afea53b2743a
Will Roper <wjr21@sussex.ac.uk>
2020-03-24 14:54:44 +0000

    added Webb image script and dependancies

--- images.py
+++ images.py
@@ -350,7 +350,8 @@ for i in range(len(reg_snaps)):
     if 'stellardata_reg' + reg + '_snap' + snap + '_npartgreaterthan' + str(npart_lim) + '.pck' in files:
         load = True
     else:
-        load = False
+        continue
+        # load = False
 
     try:
         img_main(path, snap, reg, arc_res, model, F, output=True, psf=True, npart_lim=npart_lim, dim=100, load=load,
