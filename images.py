a5d4ca2c4e9bf15e21bb3aee3396228aa69c9992
Will Roper <wjr21@sussex.ac.uk>
2020-03-24 15:55:53 +0000

    added Webb image script and dependancies

--- images.py
+++ images.py
@@ -356,12 +356,14 @@ for i in range(len(reg_snaps)):
         continue
         # load = False
 
-    try:
-        img_main(path, snap, reg, arc_res, model, F, output=True, psf=True, npart_lim=npart_lim, dim=width, load=load,
-                 conv=(u.solMass/u.Mpc**2).to(u.g/u.cm**2), scale=0.1, NIRCfs=NIRCfs)
-    except ValueError:
-        continue
-    except KeyError:
-        continue
-    except OSError:
-        continue
+    # try:
+    #     img_main(path, snap, reg, arc_res, model, F, output=True, psf=True, npart_lim=npart_lim, dim=width, load=load,
+    #              conv=(u.solMass/u.Mpc**2).to(u.g/u.cm**2), scale=0.1, NIRCfs=NIRCfs)
+    # except ValueError:
+    #     continue
+    # except KeyError:
+    #     continue
+    # except OSError:
+    #     continue
+    img_main(path, snap, reg, arc_res, model, F, output=True, psf=True, npart_lim=npart_lim, dim=width, load=load,
+             conv=(u.solMass / u.Mpc ** 2).to(u.g / u.cm ** 2), scale=0.1, NIRCfs=NIRCfs)
