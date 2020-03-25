996cbc46d8c3ee7aa6dfec525c1674a745305659
Will Roper <wjr21@sussex.ac.uk>
2020-03-25 18:10:04 +0000

    added webb image script

--- images.py
+++ images.py
@@ -387,14 +387,14 @@ for i in range(len(reg_snaps)):
     snap = reg_snaps[i][1]
     path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data/'
 
-    # files = os.listdir('UVimg_data/')
-    # print(files)
-    #
-    # if 'stellardata_reg' + reg + '_snap' + snap + '_npartgreaterthan' + str(npart_lim) + '.pck' in files:
-    #     load = True
-    # else:
-    #     load = False
-    load = False
+    files = os.listdir('UVimg_data/')
+    print(files)
+
+    if 'stellardata_reg' + reg + '_snap' + snap + '_npartgreaterthan' + str(npart_lim) + '.pck' in files:
+        load = True
+    else:
+        continue
+        # load = False
 
     # try:
     #     img_main(path, snap, reg, arc_res, model, F, output=True, psf=True, npart_lim=npart_lim, dim=width, load=load,
