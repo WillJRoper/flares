a111a70a4629477f43fd43359669433b1c2ac215
Will Roper <wjr21@sussex.ac.uk>
2020-04-07 18:01:59 +0100

    Simplified data extraction

--- UV_images.py
+++ UV_images.py
@@ -304,38 +304,44 @@ def img_main(path, snap, reg, res, npart_lim=10**3, dim=0.1, load=True, conv=1, 
             log_im6 = np.log10(galimgs[key]['SFR'])
             plt_im6[np.where(log_im6 > -np.inf)] = log_im6[np.where(log_im6 > -np.inf)]
 
-            im1 = ax1.imshow(plt_im1, extent=extents[key], cmap='viridis')
-            im2 = ax2.imshow(plt_im2, extent=extents[key], cmap='viridis')
-            im3 = ax3.imshow(plt_im3, extent=extents[key], cmap='viridis')
-            im4 = ax4.imshow(plt_im4, extent=extents[key], cmap='viridis')
-            im5 = ax5.imshow(plt_im5, extent=extents[key], cmap='viridis')
-            im6 = ax6.imshow(plt_im6, extent=extents[key], cmap='viridis')
+            ax1.imshow(np.zeros_like(plt_im1), extent=extents[key], cmap='Greys_r')
+            ax2.imshow(np.zeros_like(plt_im2), extent=extents[key], cmap='Greys_r')
+            ax3.imshow(np.zeros_like(plt_im3), extent=extents[key], cmap='Greys_r')
+            ax4.imshow(np.zeros_like(plt_im4), extent=extents[key], cmap='Greys_r')
+            ax5.imshow(np.zeros_like(plt_im5), extent=extents[key], cmap='Greys_r')
+
+            im1 = ax1.imshow(plt_im1, extent=extents[key], cmap='rainbow')
+            im2 = ax2.imshow(plt_im2, extent=extents[key], cmap='rainbow')
+            im3 = ax3.imshow(plt_im3, extent=extents[key], cmap='rainbow')
+            im4 = ax4.imshow(plt_im4, extent=extents[key], cmap='rainbow')
+            im5 = ax5.imshow(plt_im5, extent=extents[key], cmap='rainbow')
+            im6 = ax6.imshow(plt_im6, extent=extents[key], cmap='rainbow')
 
             # Draw scale line
             right_side = dim - (dim * 0.1)
             vert = dim - (dim * 0.15)
             lab_vert = vert + (dim * 0.1) * 5 / 8
             lab_horz = right_side - scale / 2
-            ax1.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
-            ax2.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
-            ax3.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
-            ax4.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
-            ax5.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
-            ax6.plot([right_side - scale, right_side], [vert, vert], color='k', linewidth=0.5)
+            ax1.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
+            ax2.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
+            ax3.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
+            ax4.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
+            ax5.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
+            ax6.plot([right_side - scale, right_side], [vert, vert], color='w', linewidth=0.5)
 
             # Label scale
             ax1.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
             ax2.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
             ax3.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
             ax4.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
             ax5.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
             ax6.text(lab_horz, lab_vert, str(int(scale*1e3)) + ' ckpc', horizontalalignment='center',
-                     fontsize=2, color='k')
+                     fontsize=2, color='w')
 
             # # Draw text
             # ax1.text(0.8, 0.9, 'Mass', bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
@@ -386,41 +392,41 @@ def img_main(path, snap, reg, res, npart_lim=10**3, dim=0.1, load=True, conv=1, 
             cbar6 = fig.colorbar(im6, cax=cax6, orientation="horizontal")
 
             # Label colorbars
-            cbar1.ax.set_xlabel(r'$\log_{10}(M_{\star}/M_{\odot})$', fontsize=2, color='k', labelpad=1.0)
+            cbar1.ax.set_xlabel(r'$\log_{10}(M_{\star}/M_{\odot})$', fontsize=2, color='w', labelpad=1.0)
             cbar1.ax.xaxis.set_label_position('top')
-            cbar1.outline.set_edgecolor('k')
+            cbar1.outline.set_edgecolor('w')
             cbar1.outline.set_linewidth(0.05)
-            cbar1.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
-            cbar2.ax.set_xlabel(r'$\log_{10}(Z_{\mathrm{los}}/[\mathrm{g}/\mathrm{ccm}^{2}])$', fontsize=2, color='k',
+            cbar1.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
+            cbar2.ax.set_xlabel(r'$\log_{10}(Z_{\mathrm{los}}/[\mathrm{g}/\mathrm{ccm}^{2}])$', fontsize=2, color='w',
                                 labelpad=1.0)
             cbar2.ax.xaxis.set_label_position('top')
-            cbar2.outline.set_edgecolor('k')
+            cbar2.outline.set_edgecolor('w')
             cbar2.outline.set_linewidth(0.05)
-            cbar2.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
-            cbar3.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{V}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='k',
+            cbar2.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
+            cbar3.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{V}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='w',
                                 labelpad=1.0)
             cbar3.ax.xaxis.set_label_position('top')
-            cbar3.outline.set_edgecolor('k')
+            cbar3.outline.set_edgecolor('w')
             cbar3.outline.set_linewidth(0.05)
-            cbar3.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
-            cbar4.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{NUV}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='k',
+            cbar3.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
+            cbar4.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{NUV}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='w',
                                 labelpad=1.0)
             cbar4.ax.xaxis.set_label_position('top')
-            cbar4.outline.set_edgecolor('k')
+            cbar4.outline.set_edgecolor('w')
             cbar4.outline.set_linewidth(0.05)
-            cbar4.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
-            cbar5.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{FUV}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='k',
+            cbar4.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
+            cbar5.ax.set_xlabel(r'$\log_{10}(L_{\mathrm{FUV}}/[\mathrm{erg}/\mathrm{s}/\mathrm{Hz}])$', fontsize=2, color='w',
                                 labelpad=1.0)
             cbar5.ax.xaxis.set_label_position('top')
-            cbar5.outline.set_edgecolor('k')
+            cbar5.outline.set_edgecolor('w')
             cbar5.outline.set_linewidth(0.05)
-            cbar5.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
-            cbar6.ax.set_xlabel(r'$\log_{10}(\mathrm{SFR}/[\mathrm{M}_{\star}/\mathrm{yr}])$', fontsize=2, color='k',
+            cbar5.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
+            cbar6.ax.set_xlabel(r'$\log_{10}(\mathrm{SFR}/[\mathrm{M}_{\star}/\mathrm{yr}])$', fontsize=2, color='w',
                                 labelpad=1.0)
             cbar6.ax.xaxis.set_label_position('top')
-            cbar6.outline.set_edgecolor('k')
+            cbar6.outline.set_edgecolor('w')
             cbar6.outline.set_linewidth(0.05)
-            cbar6.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='k', labelcolor='k')
+            cbar6.ax.tick_params(axis='x', length=1, width=0.2, pad=0.01, labelsize=2, color='w', labelcolor='w')
 
             fig.savefig('plots/UVimages/UV_reg' + str(reg) + '_snap' + snap +
                         '_gal' + str(id).split('.')[0] + 'p' + str(id).split('.')[1] + 'm_%.2f'%m
