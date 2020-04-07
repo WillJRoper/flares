30deaf863c77203bb6aef99cafbab0b5bc51c092
Will Roper <wjr21@sussex.ac.uk>
2020-04-07 20:16:41 +0100

    Simplified data extraction

--- visualisation/single_sphere.py
+++ visualisation/single_sphere.py
@@ -78,7 +78,7 @@ for reg in range(0, 40):
 # Define snapshots
 snaps = ['000_z015p000', '001_z014p000', '002_z013p000', '003_z012p000', '004_z011p000', '005_z010p000',
          '006_z009p000', '007_z008p000', '008_z007p000', '009_z006p000', '010_z005p000', '011_z004p770']
-./
+
 # Define a list of regions and snapshots
 reg_snaps = []
 regcents = []
