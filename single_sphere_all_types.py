438a22a7b882e081e44cc39a6b31ec4a5b33a5a6
Will Roper <wjr21@sussex.ac.uk>
2020-04-08 19:03:40 +0100

    Added all particle visualisation

--- single_sphere_all_types.py
+++ single_sphere_all_types.py
@@ -117,11 +117,11 @@ def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
 
     # Define particles
     qv_gas = QuickView(poss_gas, mass=masses_gas, hsml=smls_gas, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=1000, ysize=1000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
     qv_DM = QuickView(poss_DM, mass=masses_DM, hsml=smls_DM, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=1000, ysize=1000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
     qv_stars = QuickView(poss_stars, mass=masses_stars, hsml=smls_stars, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=1000, ysize=1000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
 
     # Get colomaps
     cmap_gas = ml.cm.magma
@@ -179,9 +179,9 @@ def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
 
     blend1 = Blend.Blend(rgb_DM, rgb_gas)
     dmgas_output = blend1.Overlay()
-    # blend2 = Blend.Blend(dmgas_output, rgb_stars)
-    # rgb_output = blend2.Overlay()
-    rgb_output = dmgas_output
+    blend2 = Blend.Blend(dmgas_output, rgb_stars)
+    rgb_output = blend2.Overlay()
+    # rgb_output = dmgas_output
 
     fig = plt.figure(figsize=(7, 7))
     ax = fig.add_subplot(111)
