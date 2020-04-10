55eb7246221fdb5f8a9d903d63dc5611004fa4c2
Will Roper <wjr21@sussex.ac.uk>
2020-04-10 19:45:56 +0100

    All changes

--- flythrough_animation.py
+++ flythrough_animation.py
@@ -2,7 +2,8 @@
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
-from sphviewer.tools import cmaps, Blend, QuickView
+import sphviewer as sph
+from sphviewer.tools import cmaps, Blend, camera_tools
 import matplotlib as ml
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -57,12 +58,12 @@ def get_normalised_image(img, vmin=None, vmax=None):
 def get_sphere_data(path, snap, part_type, soft):
 
     # Get positions masses and smoothing lengths
-    poss = E.read_array('SNAP', path, snap, 'PartType' + str(part_type) + '/Coordinates',
+    poss = E.read_array('PARTDATA', path, snap, 'PartType' + str(part_type) + '/Coordinates',
                         noH=True, numThreads=8)
     if part_type != 1:
-        masses = E.read_array('SNAP', path, snap, 'PartType' + str(part_type) + '/Mass',
+        masses = E.read_array('PARTDATA', path, snap, 'PartType' + str(part_type) + '/Mass',
                               noH=True, numThreads=8) * 10 ** 10
-        smls = E.read_array('SNAP', path, snap, 'PartType' + str(part_type) + '/SmoothingLength',
+        smls = E.read_array('PARTDATA', path, snap, 'PartType' + str(part_type) + '/SmoothingLength',
                             noH=True, numThreads=8)
     else:
         masses = np.ones(poss.shape[0])
@@ -71,7 +72,7 @@ def get_sphere_data(path, snap, part_type, soft):
     return poss, masses, smls
 
 
-def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
+def single_sphere(reg, snap, soft):
 
     # Define path
     path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
@@ -79,7 +80,12 @@ def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
     # Get plot data
     poss_gas, masses_gas, smls_gas = get_sphere_data(path, snap, part_type=0, soft=None)
     poss_DM, masses_DM, smls_DM = get_sphere_data(path, snap, part_type=1, soft=soft)
-    poss_stars, masses_stars, smls_stars = get_sphere_data(path, snap, part_type=4, soft=None)
+    
+    # Get centres of groups
+    grp_cops = E.read_array('SUBFIND', path, snap, 'FOF/GroupCentreOfPotential',
+                            noH=True, numThreads=8)
+    grp_ms = E.read_array('SUBFIND', path, snap, 'FOF/GroupMass',
+                            noH=True, numThreads=8)
 
     # Get the spheres centre
     centre, radius, mindist = spherical_region(path, snap)
@@ -87,12 +93,10 @@ def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
     # Centre particles
     poss_gas -= centre
     poss_DM -= centre
-    poss_stars -= centre
 
     # Remove boundary particles
     rgas = np.linalg.norm(poss_gas, axis=1)
     rDM = np.linalg.norm(poss_DM, axis=1)
-    rstars = np.linalg.norm(poss_stars, axis=1)
     okinds_gas = rgas < 14 / 0.677
     poss_gas = poss_gas[okinds_gas, :]
     masses_gas = masses_gas[okinds_gas]
@@ -101,98 +105,83 @@ def single_sphere(reg, snap, part_type, soft, t=0, p=0, num=0):
     poss_DM = poss_DM[okinds_DM, :]
     masses_DM = masses_DM[okinds_DM]
     smls_DM = smls_DM[okinds_DM]
-    okinds_stars = rstars < 14 / 0.677
-    poss_stars = poss_stars[okinds_stars, :]
-    masses_stars = masses_stars[okinds_stars]
-    smls_stars = smls_stars[okinds_stars]
 
     print('There are', len(masses_gas), 'gas particles in the region')
     print('There are', len(masses_DM), 'DM particles in the region')
-    print('There are', len(masses_stars), 'star particles in the region')
 
-    # fig = plt.figure(1, figsize=(7, 7))
+    # Set up particle objects
+    P_DM = sph.Particles(poss_DM, mass=masses_DM, hsml=smls_DM)
+    P_gas = sph.Particles(poss_gas, mass=masses_gas, hsml=smls_gas)
+
+    # Initialise the scene
+    S_DM = sph.Scene(P_DM)
+    S_gas = sph.Scene(P_gas)
+
+    # Define targets
+    targets = [[0, 0, 0]]
+    targets.append(grp_cops[np.argmax(grp_ms)])
 
     # Define the box size
     lbox = (15 / 0.677) * 2
 
-    # Define particles
-    qv_gas = QuickView(poss_gas, mass=masses_gas, hsml=smls_gas, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
-    qv_DM = QuickView(poss_DM, mass=masses_DM, hsml=smls_DM, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
-    qv_stars = QuickView(poss_stars, mass=masses_stars, hsml=smls_stars, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
-                       xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+    # Define anchors
+    anchors = {}
+    anchors['id_frames'] = [0, 180, 750, 840, 930, 1500, 1680]
+    anchors['id_targets'] = [0, 'pass', 1, 'same', 'same', 'pass', 0]
+    anchors['r'] = [lbox * 3/4, 'pass', lbox / 10, 'same', 'same', 'pass', lbox * 3/4]
+    anchors['t'] = [0, 'pass', 'pass', 180, 'pass', 'pass', 0]
+    anchors['p'] = [0, 'pass', 350, 'pass', 0, 'pass', 359]
 
-    # Get colomaps
-    cmap_gas = ml.cm.plasma
-    cmap_dm = ml.cm.Greys_r
-    cmap_stars = ml.cm.Greys_r
+    # Define the camera trajectory
+    data = camera_tools.get_camera_trajectory(targets, anchors)
 
-    # Get each particle type image
-    imgs = {'gas': qv_gas.get_image(), 'dm': qv_DM.get_image(), 'stars': qv_stars.get_image()}
-    extents = {'gas': qv_gas.get_extent(), 'dm': qv_DM.get_extent(), 'stars': qv_stars.get_extent()}
+    num = 0
+    for i in data:
+        i['xsize'] = 500
+        i['ysize'] = 500
+        i['roll'] = 0
+        S = sph.Scene(P_DM)
+        S.update_camera(**i)
+        R = sph.Render(S)
+        R.set_logscale()
+        img = R.get_image()
+        plt.imsave('plots/spheres/All/all_parts_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png'%num, img,
+                   vmin=img[np.where(img != 0)].min(), vmax=img.max(), cmap='cubehelix')
+        num += 1
 
-    # Convert images to rgb arrays
-    rgb_gas = cmap_gas(get_normalised_image(np.log10(imgs['gas']),
-                                            vmin=np.log10(imgs['gas'][np.where(imgs['gas'] != 0.0)].min()+7)))
-    rgb_DM = cmap_dm(get_normalised_image(np.log10(imgs['dm']),
-                                          vmin=np.log10(imgs['dm'][np.where(imgs['dm'] != 0.0)].min()-0.5)))
-    rgb_stars = cmap_stars(get_normalised_image(np.log10(imgs['stars']),
-                                                vmin=np.log10(imgs['stars'][np.where(imgs['stars'] != 0.0)].min())))
-
-    # rgb_gas[rgb_gas == 0.0] = np.nan
-    # rgb_DM[rgb_DM == 0.0] = np.nan
-    # rgb_stars[rgb_stars == 0.0] = np.nan
-
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111)
-
-    ax.imshow(rgb_gas, extent=extents['gas'], origin='lower')
-    ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
-                   labeltop=False, labelright=False, labelbottom=False)
-
-    fig.savefig('plots/spheres/Gas/Gas_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png' % num,
-                bbox_inches='tight')
-    plt.close(fig)
-
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111)
-
-    ax.imshow(rgb_DM, extent=extents['dm'], origin='lower')
-    ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
-                   labeltop=False, labelright=False, labelbottom=False)
-
-    fig.savefig('plots/spheres/DM/DM_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png' % num,
-                bbox_inches='tight')
-    plt.close(fig)
-
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111)
-
-    ax.imshow(rgb_stars, extent=extents['stars'], origin='lower')
-    ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
-                   labeltop=False, labelright=False, labelbottom=False)
-
-    fig.savefig('plots/spheres/Stars/Stars_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png' % num,
-                bbox_inches='tight')
-    plt.close(fig)
-
-    blend1 = Blend.Blend(rgb_DM, rgb_gas)
-    dmgas_output = blend1.Overlay()
-    # blend2 = Blend.Blend(dmgas_output, rgb_stars)
-    # rgb_output = blend2.Overlay()
-    rgb_output = dmgas_output
-
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111)
-
-    ax.imshow(rgb_output, extent=extents['gas'], origin='lower')
-    ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
-                   labeltop=False, labelright=False, labelbottom=False)
-
-    fig.savefig('plots/spheres/All/all_parts_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png'%num,
-                bbox_inches='tight')
-    plt.close(fig)
+    # # Define particles
+    # qv_gas = QuickView(poss_gas, mass=masses_gas, hsml=smls_gas, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
+    #                    xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+    # qv_DM = QuickView(poss_DM, mass=masses_DM, hsml=smls_DM, plot=False, r=lbox * 3/4, t=t, p=p, roll=0,
+    #                    xsize=5000, ysize=5000, x=0, y=0, z=0, extent=[-lbox / 2., lbox / 2., -lbox / 2., lbox / 2.])
+    #
+    # # Get colomaps
+    # cmap_gas = ml.cm.plasma
+    # cmap_dm = ml.cm.Greys_r
+    #
+    # # Get each particle type image
+    # imgs = {'gas': qv_gas.get_image(), 'dm': qv_DM.get_image()}
+    # extents = {'gas': qv_gas.get_extent(), 'dm': qv_DM.get_extent()}
+    #
+    # # Convert images to rgb arrays
+    # rgb_gas = cmap_gas(get_normalised_image(np.log10(imgs['gas']),
+    #                                         vmin=np.log10(imgs['gas'][np.where(imgs['gas'] != 0.0)].min()+7)))
+    # rgb_DM = cmap_dm(get_normalised_image(np.log10(imgs['dm']),
+    #                                       vmin=np.log10(imgs['dm'][np.where(imgs['dm'] != 0.0)].min()-0.5)))
+    #
+    # blend = Blend.Blend(rgb_DM, rgb_gas)
+    # rgb_output = blend.Overlay()
+    #
+    # fig = plt.figure(figsize=(7, 7))
+    # ax = fig.add_subplot(111)
+    #
+    # ax.imshow(rgb_output, extent=extents['gas'], origin='lower')
+    # ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
+    #                labeltop=False, labelright=False, labelbottom=False)
+    #
+    # fig.savefig('plots/spheres/All/all_parts_single_sphere_reg' + reg + '_snap' + snap + '_angle%05d.png'%num,
+    #             bbox_inches='tight')
+    # plt.close(fig)
 
 
 # Define softening lengths
