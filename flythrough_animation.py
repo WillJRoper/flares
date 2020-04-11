df19699820c70e8b4290525af581fdd27c29058f
Will Roper <wjr21@sussex.ac.uk>
2020-04-11 02:47:15 +0100

    All changes

--- flythrough_animation.py
+++ flythrough_animation.py
@@ -176,9 +176,9 @@ def single_sphere(reg, snap, soft, num):
     img_gas = R_gas.get_image()
 
     vmax_DM = img_DM.max()
-    vmin_DM = vmax_DM * 0.45
+    vmin_DM = vmax_DM * 0.5
     vmax_gas = img_gas.max()
-    vmin_gas = vmax_gas * 0.4
+    vmin_gas = vmax_gas * 0.5
 
     # Get colormaps
     cmap_gas = ml.cm.magma
