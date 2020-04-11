8be47a9ab63b87b2c521f60095f552f04be37fc5
Will Roper <wjr21@sussex.ac.uk>
2020-04-11 21:33:31 +0100

    Lnu grid creation corrected

--- halflightrad_rest.py
+++ halflightrad_rest.py
@@ -72,12 +72,12 @@ def calc_light_mass_rad(poss, ls, ms):
     sinds = np.argsort(rs)
     rs = rs[sinds]
     ls = ls[sinds]
-    ls = ls[rs < 30/1e3]
-    rs = rs[rs < 30/1e3]
-    ms = ms[rs < 30/1e3]
+    # ls = ls[rs < 30/1e3]
+    # rs = rs[rs < 30/1e3]
+    # ms = ms[rs < 30/1e3]
 
-    if len(ls) < 20:
-        return 0.0, 0.0
+    # if len(ls) < 20:
+    #     return 0.0, 0.0
 
     # Get the cumalative sum of masses
     l_profile = np.cumsum(ls)
@@ -134,10 +134,10 @@ def hl_main(snap, reg, model, F, f, npart_lim=0, conv=1, i=0, j=1, dust=False):
         # print('Computing luminosities for', id, f)
 
         # Get the luminosities
-        # if len(gas_ms[id]) == 0:
-        #     continue
-        # if np.sum(gal_ms) < 1e8:
-        #     continue
+        if len(gas_ms[id]) == 0:
+            continue
+        if np.sum(gal_ms) < 1e8:
+            continue
         gas_poss = all_gas_poss[id]
         gal_poss = all_gal_poss[id]
         # means[id] = np.mean(gal_poss, axis=0)
