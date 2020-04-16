857ac34265d8ca7820c06d3a35d778a8101f2d55
Will Roper <wjr21@sussex.ac.uk>
2020-04-16 11:57:10 +0100

    Added script to edit frames

--- flythrough_animation_frame_adjust.py
+++ flythrough_animation_frame_adjust.py
@@ -31,12 +31,10 @@ def single_sphere(reg, snap, num):
     img_gas = np.load('animationdata/gas_animationdata_reg' + reg + '_snap' + snap + '_angle%05d.npy'%num)
     img_dm = np.load('animationdata/dm_animationdata_reg' + reg + '_snap' + snap + '_angle%05d.npy'%num)
 
-    print(img_gas)
-
     # Contrast stretching
-    p2, p98 = np.percentile(img_gas, (16, 98))
+    p2, p98 = np.percentile(img_gas, (40, 99))
     img_gas = exposure.rescale_intensity(img_gas, in_range=(p2, p98))
-    p2, p98 = np.percentile(img_dm, (16, 98))
+    p2, p98 = np.percentile(img_dm, (40, 99))
     img_dm = exposure.rescale_intensity(img_dm, in_range=(p2, p98))
 
     # Set up colormaps
