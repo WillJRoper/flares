4546acde567cadb2f19db42a7a2e61ff49d28a8b
Will Roper <wjr21@sussex.ac.uk>
2020-04-17 14:46:58 +0100

    Added script to edit frames

--- change_in_size.py
+++ change_in_size.py
@@ -16,7 +16,7 @@ def get_change_in_radius(snap, prog_snap, savepath, gal_data, gals):
 
     # Open graph file
     hdf = h5py.File(savepath + 'SubMgraph_' + snap + '_PartType1.hdf5', 'r')
-
+    print(list(hdf.keys()))
     # Initialise arrays for results
     delta_hmrs = np.zeros(len(gals))
     delta_ms = np.zeros(len(gals))
