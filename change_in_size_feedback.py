a2e727c0ab7e53c11073d3259e3616ba2ebb4eb1
Will Roper <wjr21@sussex.ac.uk>
2020-04-24 20:49:59 +0100

    Updated eagle_io read in

--- change_in_size_feedback.py
+++ change_in_size_feedback.py
@@ -224,8 +224,7 @@ def main_change(masslim=1e8, hmrcut=False):
 
         if len(xs_plt) > 0:
             cbar = ax.hexbin(xs_plt, delta_hmr_plt, C=fbs_plt, gridsize=100, mincnt=1, xscale='log', yscale='log',
-                             norm=LogNorm(),
-                             linewidths=0.2, cmap='viridis')
+                             norm=LogNorm(), linewidths=0.2, cmap='viridis')
 
         ax.text(0.8, 0.9, f'$z={z}$', bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
                 transform=ax.transAxes, horizontalalignment='right', fontsize=8)
@@ -360,7 +359,7 @@ def main_change(masslim=1e8, hmrcut=False):
         delta_hmr_plt = delta_hmr_plt[okinds]
         fbs_plt = fbs_plt[okinds]
 
-        if len(xs_plt) > 0:
+        if len(fbs_plt) > 0:
             cbar = ax.hexbin(fbs_plt, delta_hmr_plt, gridsize=100, mincnt=1, xscale='log', yscale='log',
                              norm=LogNorm(), linewidths=0.2, cmap='viridis')
 
@@ -399,5 +398,5 @@ def main_change(masslim=1e8, hmrcut=False):
     fig.savefig('plots/change_in_halfmassradiusvsfeedback.png', bbox_inches='tight')
 
 
-main_change(masslim=10**9.5)
+main_change(masslim=10**8)
 
