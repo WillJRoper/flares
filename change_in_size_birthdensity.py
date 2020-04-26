5d47b0d83bab3165251769d62da96b7b9ada1127
Will Roper <wjr21@sussex.ac.uk>
2020-04-26 18:59:38 +0100

    updated feedback plots

--- change_in_size_birthdensity.py
+++ change_in_size_birthdensity.py
@@ -19,7 +19,7 @@ sns.set_style('whitegrid')
 def plot_meidan_stat(xs, ys, ax, bins=None):
 
     if bins == None:
-        bin = np.histogram_bin_edges(xs, bins='doane')
+        bin = 50
     else:
         bin = bins
 
