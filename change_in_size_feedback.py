12b31fd6d143acf1cf51254db12185c6622efa2d
Will Roper <wjr21@sussex.ac.uk>
2020-04-27 12:35:11 +0100

    updated feedback plots

--- change_in_size_feedback.py
+++ change_in_size_feedback.py
@@ -20,13 +20,13 @@ sns.set_style('whitegrid')
 def plot_meidan_stat(xs, ys, ax, bins=None):
 
     if bins == None:
-        bin = 20
+        bin = np.logspace(xs.min(), xs.max(), 20)
     else:
         bin = bins
 
     # Compute binned statistic
     y_stat, binedges, n_inbin = binned_statistic(xs, ys, statistic='median', bins=bin)
-    print(y_stat, binedges, n_inbin)
+
     # Compute bincentres
     bin_wid = binedges[1] - binedges[0]
     bin_cents = binedges[1:] - bin_wid / 2
@@ -94,7 +94,7 @@ def get_change_in_radius(snap, prog_snap, savepath, gal_data, gals, feedback, pa
     return delta_hmrs[delta_ms < 2**30], delta_ms[delta_ms < 2**30], delta_fbs[delta_ms < 2**30]
 
 
-def main_change(masslim=1e8, hmrcut=False):
+def main_change(masslim=1e8, hmrcut=False, load=False):
 
     regions = []
     for reg in range(0, 40):
@@ -103,10 +103,6 @@ def main_change(masslim=1e8, hmrcut=False):
         else:
             regions.append(str(reg))
 
-    delta_hmr_dict = {}
-    delta_ms_dict = {}
-    fbs_dict = {}
-
     # Define snapshots
     snaps = ['003_z012p000', '004_z011p000', '005_z010p000',
              '006_z009p000', '007_z008p000', '008_z007p000',
@@ -114,121 +110,136 @@ def main_change(masslim=1e8, hmrcut=False):
     prog_snaps = ['002_z013p000', '003_z012p000', '004_z011p000',
                   '005_z010p000', '006_z009p000', '007_z008p000',
                   '008_z007p000', '009_z006p000', '010_z005p000']
-    axlims_x = []
-    axlims_y = []
 
-    for snap in snaps:
+    if load:
 
-        delta_hmr_dict[snap] = {}
-        delta_ms_dict[snap] = {}
-        fbs_dict[snap] = {}
+        with open('changeinsize_feedback.pck', 'rb') as pfile1:
+            save_dict = pickle.load(pfile1)
+        delta_hmr_dict = save_dict['hmr']
+        delta_ms_dict = save_dict['ms']
+        fbs_dict = save_dict['fb']
 
-    for reg in regions:
+    else:
 
-        for snap, prog_snap in zip(snaps, prog_snaps):
+        delta_hmr_dict = {}
+        delta_ms_dict = {}
+        fbs_dict = {}
 
-            savepath = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/MergerGraphs/GEAGLE_' + reg + '/'
+        for snap in snaps:
 
-            path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
+            delta_hmr_dict[snap] = {}
+            delta_ms_dict[snap] = {}
+            fbs_dict[snap] = {}
 
-            print(reg, snap)
+        for reg in regions:
 
-            # Get redshifts
-            z_str = snap.split('z')[1].split('p')
-            z = float(z_str[0] + '.' + z_str[1])
-            z_str = prog_snap.split('z')[1].split('p')
-            prog_z = float(z_str[0] + '.' + z_str[1])
+            for snap, prog_snap in zip(snaps, prog_snaps):
+
+                savepath = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/MergerGraphs/GEAGLE_' + reg + '/'
+
+                path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
+
+                print(reg, snap)
+
+                # Get redshifts
+                z_str = snap.split('z')[1].split('p')
+                z = float(z_str[0] + '.' + z_str[1])
+                z_str = prog_snap.split('z')[1].split('p')
+                prog_z = float(z_str[0] + '.' + z_str[1])
 
 
-            # Get the particle IDs
-            part_subgrp_ids = E.read_array('PARTDATA', path, snap, 'PartType4/SubGroupNumber', numThreads=8)
-            part_grp_ids = E.read_array('PARTDATA', path, snap, 'PartType4/GroupNumber', numThreads=8)
-            
-            # Get halo IDs and halo data
-            try:
-                subgrp_ids = E.read_array('SUBFIND', path, snap, 'Subhalo/SubGroupNumber', numThreads=8)
-                grp_ids = E.read_array('SUBFIND', path, snap, 'Subhalo/GroupNumber', numThreads=8)
-                gal_hmrs = E.read_array('SUBFIND', path, snap, 'Subhalo/HalfMassRad', noH=True,
-                                        physicalUnits=True, numThreads=8)[:, 4]
-                gal_ms = E.read_array('SUBFIND', path, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
-                                      noH=False, physicalUnits=False, numThreads=8)[:, 4] * 10**10
-                feedback = E.read_array('PARTDATA', path, snap, 'PartType4/Feedback_EnergyFraction',
-                                        noH=False, physicalUnits=False, numThreads=8)
-                a_born = E.read_array('PARTDATA', path, snap, 'PartType4/StellarFormationTime', noH=True,
-                                      physicalUnits=True, numThreads=8)
+                # Get the particle IDs
+                part_subgrp_ids = E.read_array('PARTDATA', path, snap, 'PartType4/SubGroupNumber', numThreads=8)
+                part_grp_ids = E.read_array('PARTDATA', path, snap, 'PartType4/GroupNumber', numThreads=8)
 
                 # Get halo IDs and halo data
-                prog_subgrp_ids = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/SubGroupNumber', numThreads=8)
-                prog_grp_ids = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/GroupNumber', numThreads=8)
-                prog_gal_hmrs = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/HalfMassRad', noH=True,
-                                        physicalUnits=True, numThreads=8)[:, 4]
-                prog_gal_ms = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
-                                           noH=False, physicalUnits=False, numThreads=8)[:, 4] * 10**10
-            except ValueError:
-                continue
-            except OSError:
-                continue
-            except KeyError:
-                continue
+                try:
+                    subgrp_ids = E.read_array('SUBFIND', path, snap, 'Subhalo/SubGroupNumber', numThreads=8)
+                    grp_ids = E.read_array('SUBFIND', path, snap, 'Subhalo/GroupNumber', numThreads=8)
+                    gal_hmrs = E.read_array('SUBFIND', path, snap, 'Subhalo/HalfMassRad', noH=True,
+                                            physicalUnits=True, numThreads=8)[:, 4]
+                    gal_ms = E.read_array('SUBFIND', path, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+                                          noH=False, physicalUnits=False, numThreads=8)[:, 4] * 10**10
+                    feedback = E.read_array('PARTDATA', path, snap, 'PartType4/Feedback_EnergyFraction',
+                                            noH=False, physicalUnits=False, numThreads=8)
+                    a_born = E.read_array('PARTDATA', path, snap, 'PartType4/StellarFormationTime', noH=True,
+                                          physicalUnits=True, numThreads=8)
 
-            okinds = np.logical_and(part_subgrp_ids != 1073741824, (1 / a_born) - 1 < prog_z)
-            feedback = feedback[okinds]
-            part_grp_ids = part_grp_ids[okinds]
-            part_subgrp_ids = part_subgrp_ids[okinds]
-            part_halo_ids = np.zeros(part_grp_ids.size, dtype=float)
-            for (ind, g), sg in zip(enumerate(part_grp_ids), part_subgrp_ids):
-                part_halo_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
+                    # Get halo IDs and halo data
+                    prog_subgrp_ids = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/SubGroupNumber', numThreads=8)
+                    prog_grp_ids = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/GroupNumber', numThreads=8)
+                    prog_gal_hmrs = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/HalfMassRad', noH=True,
+                                            physicalUnits=True, numThreads=8)[:, 4]
+                    prog_gal_ms = E.read_array('SUBFIND', path, prog_snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+                                               noH=False, physicalUnits=False, numThreads=8)[:, 4] * 10**10
+                except ValueError:
+                    continue
+                except OSError:
+                    continue
+                except KeyError:
+                    continue
 
-            z_str = snap.split('z')[1].split('p')
-            z = float(z_str[0] + '.' + z_str[1])
+                okinds = np.logical_and(part_subgrp_ids != 1073741824, (1 / a_born) - 1 < prog_z)
+                feedback = feedback[okinds]
+                part_grp_ids = part_grp_ids[okinds]
+                part_subgrp_ids = part_subgrp_ids[okinds]
+                part_halo_ids = np.zeros(part_grp_ids.size, dtype=float)
+                for (ind, g), sg in zip(enumerate(part_grp_ids), part_subgrp_ids):
+                    part_halo_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
 
-            # Convert inputs to physical kpc
-            convert_pMpc = 1 / (1 + z)
+                z_str = snap.split('z')[1].split('p')
+                z = float(z_str[0] + '.' + z_str[1])
 
-            # Define comoving softening length in kpc
-            csoft = 0.001802390 / 0.677 * convert_pMpc
+                # Convert inputs to physical kpc
+                convert_pMpc = 1 / (1 + z)
 
-            # Remove particles not associated to a subgroup
-            if hmrcut:
-                okinds = np.logical_and(subgrp_ids != 1073741824, np.logical_and(gal_ms > 0, gal_hmrs / csoft < 1.2))
-            else:
-                okinds = np.logical_and(subgrp_ids != 1073741824, gal_ms > 0)
-            gal_hmrs = gal_hmrs[okinds]
-            gal_ms = gal_ms[okinds]
-            grp_ids = grp_ids[okinds]
-            subgrp_ids = subgrp_ids[okinds]
-            halo_ids = np.zeros(grp_ids.size, dtype=float)
-            for (ind, g), sg in zip(enumerate(grp_ids), subgrp_ids):
-                halo_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
+                # Define comoving softening length in kpc
+                csoft = 0.001802390 / 0.677 * convert_pMpc
 
-            # Remove particles not associated to a subgroup
-            okinds = prog_subgrp_ids != 1073741824
-            prog_gal_hmrs = prog_gal_hmrs[okinds]
-            prog_gal_ms = prog_gal_ms[okinds]
-            prog_grp_ids = prog_grp_ids[okinds]
-            prog_subgrp_ids = prog_subgrp_ids[okinds]
-            prog_ids = np.zeros(prog_grp_ids.size, dtype=float)
-            for (ind, g), sg in zip(enumerate(prog_grp_ids), prog_subgrp_ids):
-                prog_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
+                # Remove particles not associated to a subgroup
+                if hmrcut:
+                    okinds = np.logical_and(subgrp_ids != 1073741824, np.logical_and(gal_ms > 0, gal_hmrs / csoft < 1.2))
+                else:
+                    okinds = np.logical_and(subgrp_ids != 1073741824, gal_ms > 0)
+                gal_hmrs = gal_hmrs[okinds]
+                gal_ms = gal_ms[okinds]
+                grp_ids = grp_ids[okinds]
+                subgrp_ids = subgrp_ids[okinds]
+                halo_ids = np.zeros(grp_ids.size, dtype=float)
+                for (ind, g), sg in zip(enumerate(grp_ids), subgrp_ids):
+                    halo_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
 
-            # Initialise galaxy data
-            gal_data = {snap: {}, prog_snap: {}}
-            for m, hmr, i in zip(gal_ms, gal_hmrs, halo_ids):
-                gal_data[snap][i] = {'m': m, 'hmr': hmr}
-            for m, hmr, i in zip(prog_gal_ms, prog_gal_hmrs, prog_ids):
-                gal_data[prog_snap][i] = {'m': m, 'hmr': hmr}
+                # Remove particles not associated to a subgroup
+                okinds = prog_subgrp_ids != 1073741824
+                prog_gal_hmrs = prog_gal_hmrs[okinds]
+                prog_gal_ms = prog_gal_ms[okinds]
+                prog_grp_ids = prog_grp_ids[okinds]
+                prog_subgrp_ids = prog_subgrp_ids[okinds]
+                prog_ids = np.zeros(prog_grp_ids.size, dtype=float)
+                for (ind, g), sg in zip(enumerate(prog_grp_ids), prog_subgrp_ids):
+                    prog_ids[ind] = float(str(int(g)) + '.%05d'%int(sg))
 
-            # Get change in stellar mass and half mass radius
-            try:
-                results_tup = get_change_in_radius(snap, prog_snap, savepath, gal_data, halo_ids[gal_ms > masslim],
-                                                   feedback, part_halo_ids)
-                delta_hmr_dict[snap][reg], delta_ms_dict[snap][reg], fbs_dict[snap][reg] = results_tup
+                # Initialise galaxy data
+                gal_data = {snap: {}, prog_snap: {}}
+                for m, hmr, i in zip(gal_ms, gal_hmrs, halo_ids):
+                    gal_data[snap][i] = {'m': m, 'hmr': hmr}
+                for m, hmr, i in zip(prog_gal_ms, prog_gal_hmrs, prog_ids):
+                    gal_data[prog_snap][i] = {'m': m, 'hmr': hmr}
 
-            except OSError:
-                continue
+                # Get change in stellar mass and half mass radius
+                try:
+                    results_tup = get_change_in_radius(snap, prog_snap, savepath, gal_data, halo_ids[gal_ms > masslim],
+                                                       feedback, part_halo_ids)
+                    delta_hmr_dict[snap][reg], delta_ms_dict[snap][reg], fbs_dict[snap][reg] = results_tup
 
-    with open('changeinsize_feedback.pck', 'wb') as pfile1:
-        pickle.dump({'hmr' : delta_hmr_dict, 'ms': delta_ms_dict, 'fb': fbs_dict}, pfile1)
+                except OSError:
+                    continue
+
+        with open('changeinsize_feedback.pck', 'wb') as pfile1:
+            pickle.dump({'hmr' : delta_hmr_dict, 'ms': delta_ms_dict, 'fb': fbs_dict}, pfile1)
+
+    axlims_x = []
+    axlims_y = []
 
     # Set up plot
     fig = plt.figure(figsize=(18, 10))
@@ -467,5 +478,5 @@ def main_change(masslim=1e8, hmrcut=False):
     fig.savefig('plots/change_in_halfmassradiusvsfeedback.png', bbox_inches='tight')
 
 
-main_change(masslim=10**8)
+main_change(masslim=10**8, load=True)
 
