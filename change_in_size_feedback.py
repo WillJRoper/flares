25cd98dfa9aa6e630d5803ea3d1777bbd85cf1f9
Will Roper <wjr21@sussex.ac.uk>
2020-04-27 13:39:44 +0100

    updated feedback plots

--- change_in_size_feedback.py
+++ change_in_size_feedback.py
@@ -267,7 +267,8 @@ def main_change(masslim=1e8, hmrcut=False, load=False):
         delta_hmr_plt = np.concatenate(list(delta_hmr_dict[snap].values()))
         fbs_plt = np.concatenate(list(fbs_dict[snap].values()))
 
-        okinds = np.logical_and(xs_plt > 0, np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
+        okinds = np.logical_and(np.logical_and(xs_plt > 0, ~np.isnan(xs_plt)),
+                                np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
         xs_plt = xs_plt[okinds]
         fbs_plt = fbs_plt[okinds]
 
@@ -344,7 +345,8 @@ def main_change(masslim=1e8, hmrcut=False, load=False):
         delta_hmr_plt = np.concatenate(list(delta_hmr_dict[snap].values()))
         fbs_plt = np.concatenate(list(fbs_dict[snap].values()))
 
-        okinds = np.logical_and(xs_plt > 0, np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
+        okinds = np.logical_and(np.logical_and(xs_plt > 0, ~np.isnan(xs_plt)),
+                                np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
         xs_plt = xs_plt[okinds]
         delta_hmr_plt = delta_hmr_plt[okinds]
         fbs_plt = fbs_plt[okinds]
@@ -428,7 +430,8 @@ def main_change(masslim=1e8, hmrcut=False, load=False):
         delta_hmr_plt = np.concatenate(list(delta_hmr_dict[snap].values()))
         fbs_plt = np.concatenate(list(fbs_dict[snap].values()))
 
-        okinds = np.logical_and(xs_plt > 0, np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
+        okinds = np.logical_and(np.logical_and(xs_plt > 0, ~np.isnan(xs_plt)),
+                                np.logical_and(delta_hmr_plt > 0, ~np.isnan(fbs_plt)))
         xs_plt = xs_plt[okinds]
         delta_hmr_plt = delta_hmr_plt[okinds]
         fbs_plt = fbs_plt[okinds]
