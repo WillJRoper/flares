523fe34686f157ba7b6c0656680788c198ff0d2a
Will Roper <wjr21@sussex.ac.uk>
2020-04-27 17:30:59 +0100

    updated feedback plots

--- change_in_size_feedback.py
+++ change_in_size_feedback.py
@@ -28,30 +28,10 @@ def plot_meidan_stat(xs, ys, ax, bins=None):
 
     # Compute binned statistic
     y_stat, binedges, n_inbin = binned_statistic(xs, ys, statistic='median', bins=bin)
-
+    print(y_stat.shape, binedges.shape, n_inbin.shape)
     # Compute bincentres
     bin_wid = binedges[1] - binedges[0]
     bin_cents = binedges[1:] - bin_wid / 2
-
-    okinds = np.logical_and(~np.isnan(bin_cents), ~np.isnan(y_stat))
-
-    y_stats = []
-    bin_centss = []
-    for y, b, n in zip(y_stat[okinds], bin_cents[okinds], n_inbin[okinds]):
-
-        if n < 10:
-            try:
-                y_stats[-1] = np.median([y, y_stat[-1]])
-                bin_centss[-1] = b - bin_wid
-            except IndexError:
-                y_stats.append(y)
-                bin_centss.append(b)
-        else:
-            y_stats.append(y)
-            bin_centss.append(b)
-
-    y_stats = np.array(y_stats)
-    bin_centss = np.array(bin_centss)
 
     # # Sort the results
     # sinds = np.argsort(xs)
@@ -86,7 +66,7 @@ def plot_meidan_stat(xs, ys, ax, bins=None):
     #     y_stat.pop()
     #     bin_cents.pop()
 
-    ax.plot(bin_centss, y_stats, color='r', linestyle='-')
+    ax.plot(bin_cents, y_stat, color='r', linestyle='-')
 
 
 def get_change_in_radius(snap, prog_snap, savepath, gal_data, gals, feedback, part_halo_ids):
