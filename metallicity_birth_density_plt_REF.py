10944d958c74765c5034962479f3b8c12ba6d3ee
Will Roper <wjr21@sussex.ac.uk>
2020-05-02 15:34:18 +0100

    Adapted metallitcity vs brith density script

--- metallicity_birth_density_plt_REF.py
+++ metallicity_birth_density_plt_REF.py
@@ -261,8 +261,8 @@ for ax, snap, (i, j) in zip([ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9], snaps
         ax.set_ylabel("Smoothed Metal Mass Fraction $Z$")
 
 for ax in [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9]:
-    ax.set_xlim(np.min(axlims_x), np.max(axlims_x))
-    ax.set_ylim(np.min(axlims_y), np.max(axlims_y))
+    ax.set_xlim(10**-3, 10**6.8)
+    ax.set_ylim(10**-5.9, 10**0)
     for spine in ax.spines.values():
         spine.set_edgecolor('k')
 
