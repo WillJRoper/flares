c7cf8b16e7518374f70e7d37e895fe7719df4980
Will Roper <wjr21@sussex.ac.uk>
2020-05-07 23:04:59 +0100

    bulge or disk comparison script

--- spheroid_disk.py
+++ spheroid_disk.py
@@ -6,8 +6,10 @@ import matplotlib.gridspec as gridspec
 from unyt import mh, cm, Gyr, g, Msun, Mpc
 from matplotlib.colors import LogNorm
 import numba as nb
-import flares as fl
+from flares import flares
 
+
+fl = flares
 
 @nb.njit(nogil=True, parallel=True)
 def rms_radii(pos, cent):
@@ -42,7 +44,7 @@ def get_data(snap, masslim=1e8):
     path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
 
     # Get particle IDs
-    halo_part_inds = fl.flares.get_subgroup_part_inds(path, snap, 4, all_parts=False)
+    halo_part_inds = fl.get_subgroup_part_inds(path, snap, part_type=4, all_parts=False)
 
     # Get halo IDs and halo data
     try:
