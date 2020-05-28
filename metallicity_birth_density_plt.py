a0e4cbb12974a93fe7fee32545ae3488b9fccaae
Will Roper <wjr21@sussex.ac.uk>
2020-05-28 20:52:01 +0100

    added softening length circle to mass distributions

--- metallicity_birth_density_plt.py
+++ metallicity_birth_density_plt.py
@@ -164,7 +164,7 @@ snaps = ['003_z012p000', '004_z011p000', '005_z010p000',
          '006_z009p000', '007_z008p000', '008_z007p000',
          '009_z006p000', '010_z005p000', '011_z004p770']
 
-stellar_bd_dict, stellar_met_dict = get_data(masslim=10**9.5, load=True)
+stellar_bd_dict, stellar_met_dict = get_data(masslim=10**9.5, load=False)
 
 # plt.style.use("mnras.mplstyle")
 
