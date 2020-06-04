f4e2640c05b6df21c60274cdaa4b78f469efd365
Will Roper <wjr21@sussex.ac.uk>
2020-06-04 13:03:32 +0100

    animation

--- half_mass_evolution.py
+++ half_mass_evolution.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.gridspec as gridspec
 from matplotlib.colors import LogNorm
-import eagle_IO as E
+import eagle_IO.eagle_IO as E
 import h5py
 import sys
 import multiprocessing
@@ -259,6 +259,133 @@ def main_evolve(reg, root_snap='011_z004p770', lim=1):
 
         # if count >= lim:
         #     break
+
+
+def main_evolve(reg, root_snap='011_z004p770', lim=1):
+
+    snaplist = ['000_z015p000', '001_z014p000', '002_z013p000', '003_z012p000',
+                '004_z011p000', '005_z010p000', '006_z009p000', '007_z008p000',
+                '008_z007p000', '009_z006p000', '010_z005p000', '011_z004p770']
+
+    graphpath = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/MergerGraphs/GEAGLE_' + reg + '/'
+
+    path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
+
+    # Get halo IDs and halo data
+    subgrp_ids = E.read_array('SUBFIND', path, root_snap, 'Subhalo/SubGroupNumber', numThreads=8)
+    grp_ids = E.read_array('SUBFIND', path, root_snap, 'Subhalo/GroupNumber', numThreads=8)
+    gal_hmrs = E.read_array('SUBFIND', path, root_snap, 'Subhalo/HalfMassRad', noH=True,
+                            physicalUnits=True, numThreads=8)[:, 4]
+    gal_ms = E.read_array('SUBFIND', path, root_snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+                          noH=True, physicalUnits=True, numThreads=8)[:, 4] * 10 ** 10
+
+    z_str = root_snap.split('z')[1].split('p')
+    z = float(z_str[0] + '.' + z_str[1])
+
+    # Convert inputs to physical kpc
+    convert_pMpc = 1 / (1 + z)
+
+    # Define comoving softening length in kpc
+    csoft = 0.001802390 / 0.677 * convert_pMpc
+
+    # Remove particles not associated to a subgroup
+    okinds = np.logical_and(subgrp_ids != 1073741824, np.logical_and(gal_ms > 10**9.5, gal_hmrs / csoft < 1.2))
+    gal_hmrs = gal_hmrs[okinds]
+    gal_ms = gal_ms[okinds]
+    grp_ids = grp_ids[okinds]
+    subgrp_ids = subgrp_ids[okinds]
+    halo_ids = np.zeros(grp_ids.size, dtype=float)
+    for (ind, g), sg in zip(enumerate(grp_ids), subgrp_ids):
+        halo_ids[ind] = float(str(int(g)) + '.%05d' % int(sg))
+
+    # Initialise counter
+    count = 0
+
+    for root, hr, m in zip(halo_ids, gal_hmrs, gal_ms):
+
+        count += 1
+
+        # Get the graph
+        forest = forest_worker(root, graphpath)
+
+        hmrs, masses, progs = get_evolution(forest, path, graphpath, snaplist)
+
+        forest_snaps = list(forest.keys())[1:]
+        forest_progsnaps = list(forest.keys())[:-1]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        masses_plt = []
+        hmrs_plt = []
+        snaps = []
+
+        for snap, prog_snap in zip(forest_snaps, forest_progsnaps):
+
+            z_str = snap.split('z')[1].split('p')
+            z = float(z_str[0] + '.' + z_str[1])
+
+            z_str_prog = prog_snap.split('z')[1].split('p')
+            zp = float(z_str_prog[0] + '.' + z_str_prog[1])
+
+            # Define comoving softening length in kpc
+            soft = 0.001802390 / 0.677 * 1 / (1 + z)
+            # prog_soft = 0.001802390 / 0.677 * 1 / (1 + zp)
+
+            for halo in forest[snap]:
+
+                mass = masses[snap][halo]
+                hmr = hmrs[snap][halo]
+
+                if mass == 0 or hmr == 0:
+                    continue
+
+                print(z)
+
+                masses_plt.extend(mass)
+                hmrs_plt.extend(hmr / soft)
+                snaps.append(int(snap.split('_')[0]))
+
+                # if z < 5:
+                #     ax.scatter(int(snap.split('_')[0]), hmr, s=masses / max(masses) * 50, marker='*', color='k')
+
+                # # Get prog data
+                # prog_hmrs = []
+                # prog_mass = []
+                # for prog in progs[snap][halo]:
+                #     try:
+                #         prog_hmrs.append(hmrs[prog_snap][prog])
+                #         prog_mass.append(masses[prog_snap][prog])
+                #     except KeyError:
+                #         continue
+
+                # for phmr, pm in zip(prog_hmrs, prog_mass):
+                #     if pm == 0 or phmr == 0:
+                #         continue
+                #     print(pm, phmr, mass - pm, hmr - phmr)
+                    # ax.arrow(pm[0], phmr[0], mass[0] - pm[0], hmr[0] - phmr[0])
+                    # ax.scatter(pm, phmr / prog_soft, marker='.', color='r')
+
+        masses_plt = np.array(masses_plt)
+        hmrs_plt = np.array(hmrs_plt)
+        snaps = np.array(snaps)
+
+        im = ax.scatter(snaps[masses_plt > 1e8], hmrs_plt[masses_plt > 1e8],
+                        s=masses_plt[masses_plt > 1e8] / max(masses) * 30, cmap='plasma')
+
+        ax.set_xlabel(r'$S_{\mathrm{num}}$')
+        ax.set_ylabel('$R_{1/2,\mathrm{\star}}/\epsilon$')
+
+        ax.set_yscale('log')
+
+        cbar = fig.colorbar(im)
+        cbar.ax.set_ylabel(r'$M_{\mathrm{\star}}/M_\odot$')
+
+        fig.savefig('plots/Evolution/Graph_HalfMassRadius_Mass' + str(root) + '.png',
+                    bbox_inches='tight')
+
+        if count >= lim:
+            break
 
 
 main_evolve(reg='00', root_snap='011_z004p770', lim=1)
