797b21d4cdc3ee255470bc076f55c721c4e986c4
Will Roper <wjr21@sussex.ac.uk>
2020-06-04 13:39:09 +0100

    animation

--- half_mass_evolution.py
+++ half_mass_evolution.py
@@ -78,7 +78,7 @@ def get_forest(z0halo, treepath):
 
                 # Assign progenitors adding the snapshot * 100000 to the ID to keep track of the snapshot ID
                 # in addition to the halo ID
-                forest_dict.setdefault(prog_snap, set()).update({(prog_snap, snap_tree_data[str(halo)]['Prog_haloIDs'][...])})
+                forest_dict.setdefault(prog_snap, set()).update({(prog_snap, snap_tree_data[str(halo[0])]['Prog_haloIDs'][...])})
                 snap_tree_data.close()
 
             # Add any new halos not found in found halos to the new halos set
@@ -101,7 +101,7 @@ def get_forest(z0halo, treepath):
 
                 # Load descendants adding the snapshot * 100000 to keep track of the snapshot ID
                 # in addition to the halo ID
-                forest_dict.setdefault(desc_snap, set()).update({(desc_snap, snap_tree_data[str(halo)]['Desc_haloIDs'][...])})
+                forest_dict.setdefault(desc_snap, set()).update({(desc_snap, snap_tree_data[str(halo[0])]['Desc_haloIDs'][...])})
 
                 snap_tree_data.close()
 
@@ -119,7 +119,7 @@ def get_forest(z0halo, treepath):
             del forest_dict[snap]
             continue
 
-        forest_dict[snap] = np.array([halo[1] for halo in forest_dict[snap]])
+        forest_dict[snap] = np.array([halo[0] for halo in forest_dict[snap]])
 
     return forest_dict
 
