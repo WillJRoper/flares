a99fbda2112d35fed899edf0d5e67963eb46a388
Will Roper <wjr21@sussex.ac.uk>
2020-06-08 00:31:18 +0100

    animation

--- all_sphere_all_types.py
+++ all_sphere_all_types.py
@@ -135,7 +135,7 @@ def blend(rgb_DM, rgb_gas):
     return rgb_output
 
 
-def spheregrid(snap, nframes):
+def spheregrid(snap, nframes, num):
 
     # Define region list
     regions = []
@@ -145,54 +145,40 @@ def spheregrid(snap, nframes):
         else:
             regions.append(str(reg))
 
-    for num in range(nframes):
+    fig = plt.figure(figsize=(8 * 3.95, 5 * 4))
+    gs = gridspec.GridSpec(nrows=5, ncols=8)
+    gs.update(wspace=0.0, hspace=0.0)
 
-        fig = plt.figure(figsize=(8 * 3.95, 5 * 4))
-        gs = gridspec.GridSpec(nrows=5, ncols=8)
-        gs.update(wspace=0.0, hspace=0.0)
+    pltgrid = []
+    for i in range(5):
+        for j in range(8):
+            pltgrid.append((i, j))
 
-        pltgrid = []
-        for i in range(5):
-            for j in range(8):
-                pltgrid.append((i, j))
+    for reg, (i, j) in zip(regions, pltgrid):
 
-        for reg, (i, j) in zip(regions, pltgrid):
+        print(reg, i, j)
 
-            print(reg, i, j)
+        # Load the current snapshot data
+        hdf = h5py.File(f'spheresdata/spheregird_img_data_{reg}_{snap}.hdf5', 'r')
 
-            # Load the current snapshot data
-            hdf = h5py.File(f'spheresdata/spheregird_img_data_{reg}_{snap}.hdf5', 'r')
+        ax = fig.add_subplot(gs[i, j])
 
-            ax = fig.add_subplot(gs[i, j])
+        img, extent = hdf[str(num)]['img'][...], hdf[str(num)].attrs['extent']
 
-            img, extent = hdf[str(num)]['img'][...], hdf[str(num)].attrs['extent']
+        hdf.close()
 
-            hdf.close()
+        ax.imshow(img, extent=extent, origin='lower')
+        ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
+                       labeltop=False, labelright=False, labelbottom=False)
 
-            ax.imshow(img, extent=extent, origin='lower')
-            ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
-                           labeltop=False, labelright=False, labelbottom=False)
-
-        fig.savefig('plots/spheres/All/all_parts_grid_sphere_snap' + snap + '_angle%05d.png'%num,
-                    bbox_inches='tight', facecolor='k')
-        plt.close(fig)
+    fig.savefig('plots/spheres/All/all_parts_grid_sphere_snap' + snap + '_angle%05d.png'%num,
+                bbox_inches='tight', facecolor='k')
+    plt.close(fig)
 
 
 # Define softening lengths
 csoft = 0.001802390 / 0.677
 
-# # Define region list
-# regions = []
-# for reg in range(0, 40):
-#     if reg < 10:
-#         regions.append('0' + str(reg))
-#     else:
-#         regions.append(str(reg))
-#
-# # Define snapshots
-# snaps = ['000_z015p000', '001_z014p000', '002_z013p000', '003_z012p000', '004_z011p000', '005_z010p000',
-#          '006_z009p000', '007_z008p000', '008_z007p000', '009_z006p000', '010_z005p000', '011_z004p770']
-#
 # Define region list
 regions = []
 for reg in range(0, 40):
@@ -202,7 +188,7 @@ for reg in range(0, 40):
         regions.append(str(reg))
 
 # Get colormaps
-cmap_gas = ml.cm.magma
+cmap_gas = cmaps.twilight()
 cmap_dm = ml.cm.Greys_r
 
 # Minimum corrections
@@ -210,42 +196,43 @@ dm_vmin = 7
 gas_vmin = -0.5
 
 ind = int(sys.argv[1])
-# print(reg_snaps[ind])
-# reg, snap = reg_snaps[ind]
-reg, snap = regions[ind], '010_z005p000'
+
+reg = regions[ind]
+snap = '011_z004p770'
 
 # Define path
 path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
 
-# # Get the spheres centre
-# centre, radius, mindist = spherical_region(path, snap)
+# Get the spheres centre
+centre, radius, mindist = spherical_region(path, snap)
 
 nframes = 180
 
-# # Define rotations
-# ps = np.linspace(0, 360, nframes)
-#
-# # Set up particle objects
-# dm_scene = set_up_single_sphere(reg, snap, csoft, centre, part_type=1)
-# gas_scene = set_up_single_sphere(reg, snap, csoft, centre, part_type=0)
+# Define rotations
+ps = np.linspace(0, 360, nframes)
 
-# # Load the current snapshot data
-# hdf = h5py.File(f'spheresdata/spheregird_img_data_{reg}_{snap}.hdf5', 'w')
-#
-# for num, p in enumerate(ps):
-#
-#     print(p)
-#
-#     rgb_gas, extent = get_single_sphere_imgs(gas_scene, cmap_gas, gas_vmin, p, t=0)
-#     rgb_dm, _ = get_single_sphere_imgs(dm_scene, cmap_dm, dm_vmin, p, t=0)
-#
-#     rgb = blend(rgb_dm, rgb_gas)
-#
-#     frame = hdf.create_group(str(num))
-#     frame.create_dataset('img', shape=rgb.shape, dtype=float, data=rgb,
-#                             compression='gzip')
-#     frame.attrs['extent'] = extent
-#
-# hdf.close()
+# Set up particle objects
+dm_scene = set_up_single_sphere(reg, snap, csoft, centre, part_type=1)
+gas_scene = set_up_single_sphere(reg, snap, csoft, centre, part_type=0)
 
-spheregrid(snap, nframes)
+# Load the current snapshot data
+hdf = h5py.File(f'spheresdata/spheregird_img_data_{reg}_{snap}.hdf5', 'w')
+
+for num, p in enumerate(ps):
+
+    rgb_gas, extent = get_single_sphere_imgs(gas_scene, cmap_gas, gas_vmin, p, t=0)
+    rgb_dm, _ = get_single_sphere_imgs(dm_scene, cmap_dm, dm_vmin, p, t=0)
+
+    rgb = blend(rgb_dm, rgb_gas)
+
+    frame = hdf.create_group(str(num))
+    frame.create_dataset('img', shape=rgb.shape, dtype=float, data=rgb,
+                            compression='gzip')
+    frame.attrs['extent'] = extent
+
+hdf.close()
+#
+# spheregrid(snap, nframes, ind)
+#
+# for num in range(nframes):
+#     spheregrid(snap, nframes, num)
