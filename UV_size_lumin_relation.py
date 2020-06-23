e8a877454e2a3872d6b0e50b60aa809019523075
Will Roper <wjr21@sussex.ac.uk>
2020-06-23 17:52:56 +0100

    added spurious dynamic test

--- UV_size_lumin_relation.py
+++ UV_size_lumin_relation.py
@@ -141,7 +141,7 @@ for ax, snap, (i, j) in zip([ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9], snaps
 
 for ax in [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9]:
     ax.set_xlim(np.min(axlims_x), np.max(axlims_x))
-    ax.set_ylim(np.min(axlims_y), np.max(axlims_y))
+    ax.set_ylim(np.min(axlims_y), 10**2)
     for spine in ax.spines.values():
         spine.set_edgecolor('k')
 
