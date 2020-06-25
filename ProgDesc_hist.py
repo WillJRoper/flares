d0d193d68a8606e30b0bbf8f2e8683d88e9d489d
Will Roper <wjr21@sussex.ac.uk>
2020-06-25 12:08:53 +0100

    added simple graph

--- ProgDesc_hist.py
+++ ProgDesc_hist.py
@@ -3,11 +3,50 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from matplotlib.collections import LineCollection
 import h5py
 import os
 import gc
 import sys
 matplotlib.use('Agg')
+
+
+def multiline(xs, ys, c, ax=None, **kwargs):
+    """Plot lines with different colorings
+
+    Parameters
+    ----------
+    xs : iterable container of x coordinates
+    ys : iterable container of y coordinates
+    c : iterable container of numbers mapped to colormap
+    ax (optional): Axes to plot on.
+    kwargs (optional): passed to LineCollection
+
+    Notes:
+        len(xs) == len(ys) == len(c) is the number of line segments
+        len(xs[i]) == len(ys[i]) is the number of points for each line (indexed by i)
+
+    Returns
+    -------
+    lc : LineCollection instance.
+    """
+
+    # find axes
+    ax = plt.gca() if ax is None else ax
+
+    # create LineCollection
+    segments = [np.column_stack([x, y]) for x, y in zip(xs, ys)]
+    lc = LineCollection(segments, **kwargs)
+
+    # set coloring of line segments
+    #    Note: I get an error if I pass c as a list here... not sure why.
+    lc.set_array(np.asarray(c))
+
+    # add lines to axes and rescale
+    #    Note: adding a collection doesn't autoscalee xlim/ylim
+    ax.add_collection(lc)
+    ax.autoscale()
+    return lc
 
 
 def get_ns(graphpath, snap):
@@ -89,45 +128,65 @@ def main():
     plt.close(fig)
 
     regs_ovdens = np.loadtxt("region_overdensity.txt", dtype=float)
-    reg_bins = np.digitize(regs_ovdens, bins=np.linspace(-0.5, 1, 6))
+    bin_edges = np.linspace(regs_ovdens.min(), regs_ovdens.max(), 6)
+    reg_bins = np.digitize(regs_ovdens, bins=bin_edges)
+    bin_wid = bin_edges[1] - bin_edges[0]
+    bin_cents = bin_edges[1:] - bin_wid / 2
     print(reg_bins)
     nprogs_environ = {}
     ndescs_environ = {}
 
-    # for reg in regions:
-    #
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(121)
-    # ax2 = fig.add_subplot(122)
-    #
-    # for reg in regions:
-    #
-    #     progbins, progcounts = np.unique(nprogs, return_counts=True)
-    #     descbins, desccounts = np.unique(ndescs, return_counts=True)
-    #
-    #     ax1.bar(progbins, progcounts, width=1, alpha=0.9, color='b', ec='b')
-    #     ax2.bar(descbins, desccounts, width=1, alpha=0.9, color='b', ec='b')
-    #
-    # # Set y-axis scaling to logarithmic
-    # ax1.set_yscale('log')
-    # ax2.set_yscale('log')
-    #
-    # # Ensure tick labels are integers
-    # ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-    # ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
-    #
-    # # Label axes
-    # ax1.set_xlabel(r'$N_{dProg}$')
-    # ax1.set_ylabel(r'$N$')
-    # ax2.set_xlabel(r'$N_{dDesc}$')
-    # ax2.set_ylabel(r'$N$')
-    #
-    # # Draw legend
-    # handles, labels = ax1.get_legend_handles_labels()
-    # ax1.legend(handles, labels)
-    #
-    # # Save the plot as a png
-    # fig.savefig('plots/ProgDescHist.png', bbox_inches='tight')
+    for reg, bin_ind in zip(regions, reg_bins):
+
+        nprogs_environ.setdefault(bin_ind, []).extend(nprogs_dict[reg])
+        ndescs_environ.setdefault(bin_ind, []).extend(ndescs_dict[reg])
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+
+    colors = matplotlib.cm.plasma(np.linspace(0, 1, 6))
+    plt_prog_xs = []
+    plt_prog_ys = []
+    plt_desc_xs = []
+    plt_desc_ys = []
+
+    for bin_ind, ovden in enumerate(bin_cents):
+
+        progbins, progcounts = np.unique(nprogs_environ[bin_ind], return_counts=True)
+        descbins, desccounts = np.unique(ndescs_environ[bin_ind], return_counts=True)
+
+        plt_prog_xs.extend(progbins)
+        plt_prog_ys.extend(progcounts)
+        plt_desc_xs.extend(descbins)
+        plt_desc_ys.extend(desccounts)
+
+    lc = multiline(plt_prog_xs, plt_prog_ys, bin_cents, cmap='plasma', lw=2, ax=ax1)
+    lc = multiline(plt_desc_xs, plt_desc_ys, bin_cents, cmap='plasma', lw=2, ax=ax2)
+
+    axcb = fig.colorbar(lc)
+    axcb.set_label('$\Delta$')
+
+    # Set y-axis scaling to logarithmic
+    ax1.set_yscale('log')
+    ax2.set_yscale('log')
+
+    # Ensure tick labels are integers
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Label axes
+    ax1.set_xlabel(r'$N_{dProg}$')
+    ax1.set_ylabel(r'$N$')
+    ax2.set_xlabel(r'$N_{dDesc}$')
+    ax2.set_ylabel(r'$N$')
+
+    # Draw legend
+    handles, labels = ax1.get_legend_handles_labels()
+    ax1.legend(handles, labels)
+
+    # Save the plot as a png
+    fig.savefig('plots/ProgDescHist_environ.png', bbox_inches='tight')
 
 
 main()
