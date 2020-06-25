ee9d10db81ac932804335eef3e2d79464e66bdab
Will Roper <wjr21@sussex.ac.uk>
2020-06-25 15:45:37 +0100

    added simple graph

--- starparticle_mass_hist.py
+++ starparticle_mass_hist.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.colors import LogNorm
-import eagle_IO as E
+import eagle_IO.eagle_IO as E
 import seaborn as sns
 import pickle
 import itertools
@@ -21,25 +21,24 @@ for reg in range(0, 40):
 
 snap = '010_z005p000'
 
-star_ms_dict = {}
+star_ms = []
 for reg in regions:
 
     print(reg)
 
     path = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_' + reg + '/data/'
 
-    star_ms_dict = E.read_array('PARTDATA', path, snap, 'PartType4/Mass', noH=True)
+    star_ms.extend(E.read_array('SNAP', path, snap, 'PartType4/Mass'))
 
-star_ms = np.concatenate(list(star_ms_dict))
-
-H, bins = np.histogram(star_ms, np.logspace(6, 7.5, 100))
+star_ms = np.array(star_ms)
+H, bins = np.histogram(star_ms / 0.6777, np.logspace(5, 10, 200))
 
 bin_cents = bins[1:] - ((bins[1] - bins[0]) / 2)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.plot(bin_cents, H)
+ax.bar(bin_cents, H)
 ax.set_ylabel('$N$')
 ax.set_xlabel('$M_{*}/M_{\odot}$')
 
