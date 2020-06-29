61d5db4b3c719f4fc6ae82f4e64a49a03c914649
Will Roper <wjr21@sussex.ac.uk>
2020-06-29 18:34:30 +0100

    prog analysis

---
+++ multi_bh.py
@@ -0,0 +1,117 @@
+#!/cosma/home/dp004/dc-rope1/.conda/envs/flares-env/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib
+import numba as nb
+import eagle_IO.eagle_IO as E
+from matplotlib.colors import LogNorm
+import seaborn as sns
+from flares import flares
+matplotlib.use('Agg')
+
+sns.set_style('whitegrid')
+
+
+def get_part_ids(sim, snapshot, part_type, all_parts=False):
+
+    # Get the particle IDs
+    if all_parts:
+        part_ids = E.read_array('SNAP', sim, snapshot, 'PartType' + str(part_type) + '/ParticleIDs', numThreads=8)
+    else:
+        part_ids = E.read_array('PARTDATA', sim, snapshot, 'PartType' + str(part_type) + '/ParticleIDs',
+                                numThreads=8)
+
+    # Extract the halo IDs (group names/keys) contained within this snapshot
+    group_part_ids = E.read_array('PARTDATA', sim, snapshot, 'PartType' + str(part_type) + '/ParticleIDs',
+                                  numThreads=8)
+    grp_ids = E.read_array('PARTDATA', sim, snapshot, 'PartType' + str(part_type) + '/GroupNumber',
+                           numThreads=8)
+    subgrp_ids = E.read_array('PARTDATA', sim, snapshot, 'PartType' + str(part_type) + '/SubGroupNumber',
+                              numThreads=8)
+
+    # Remove particles not associated to a subgroup
+    okinds = subgrp_ids != 1073741824
+    group_part_ids = group_part_ids[okinds]
+    grp_ids = grp_ids[okinds]
+    subgrp_ids = subgrp_ids[okinds]
+
+    # Convert IDs to float(groupNumber.SubGroupNumber) format, i.e. group 1 subgroup 11 = 1.00011
+    halo_ids = np.zeros(grp_ids.size, dtype=float)
+    for (ind, g), sg in zip(enumerate(grp_ids), subgrp_ids):
+        halo_ids[ind] = float(str(int(g)) + '.%05d' % int(sg))
+
+    # Sort particle IDs
+    unsort_part_ids = np.copy(part_ids)
+    sinds = np.argsort(part_ids)
+    part_ids = part_ids[sinds]
+
+    # Get the index of particles in the snapshot array from the in group array
+    sorted_index = np.searchsorted(part_ids, group_part_ids)
+    yindex = np.take(sinds, sorted_index, mode="raise")
+    mask = unsort_part_ids[yindex] != group_part_ids
+    result = np.ma.array(yindex, mask=mask)
+
+    # Apply mask to the id arrays
+    part_groups = halo_ids[np.logical_not(result.mask)]
+    parts_in_groups = result.data[np.logical_not(result.mask)]
+
+    # Produce a dictionary containing the index of particles in each halo
+    halo_part_inds = {}
+    for ind, grp in zip(parts_in_groups, part_groups):
+        halo_part_inds.setdefault(grp, set()).update({ind})
+
+    return halo_part_inds
+
+
+regions = []
+for reg in range(0, 40):
+    if reg < 10:
+        regions.append('0' + str(reg))
+    else:
+        regions.append(str(reg))
+
+snaps = ['000_z015p000', '001_z014p000', '002_z013p000', '003_z012p000', '004_z011p000', '005_z010p000',
+         '006_z009p000', '007_z008p000', '008_z007p000', '009_z006p000', '010_z005p000', '011_z004p770']
+
+reg_snaps = []
+for reg in reversed(regions):
+
+    for snap in snaps:
+        reg_snaps.append((reg, snap))
+
+first_bh = []
+second_bh = []
+
+for reg, snap in reg_snaps:
+
+    path = '/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/G-EAGLE_' + reg + '/data'
+
+    part_inds = get_part_ids(path, snap, part_type=5, all_parts=False)
+
+    masses = E.read_array('PARTDATA', path, snap, 'PartType5/Mass', numThreads=8) / 0.6777 * 10**10
+
+    for key in part_inds:
+        inds = part_inds[key]
+        if len(inds) < 2:
+            continue
+        ms = np.sort(masses[inds])
+        print(ms)
+        first_bh.append(ms[-1])
+        second_bh.append(ms[-2])
+
+first_bh = np.array(first_bh)
+second_bh = np.array(second_bh)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+cbar = ax.hexbin(first_bh, second_bh, gridsize=100, mincnt=1, xscale='log', yscale='log', norm=LogNorm(),
+                 linewidths=0.2, cmap='viridis')
+
+ax.set_xlabel('$M_{1}$')
+ax.set_ylabel('$M_{2}$')
+
+fig.colorbar(cbar)
+
+fig.savefig("plots/multiple_BH_probe.png", bbox_inches='tight')
