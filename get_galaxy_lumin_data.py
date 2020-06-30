4c082370568d47c774d5141c928b720d9c07da1c
Will Roper <wjr21@sussex.ac.uk>
2020-06-30 16:24:58 +0100

    prog analysis

--- get_galaxy_lumin_data.py
+++ get_galaxy_lumin_data.py
@@ -100,7 +100,7 @@ def calc_light_mass_rad(ls, rs):
     l_profile = np.cumsum(ls)
 
     # Get the total mass and half the total mass
-    tot_l = l_profile[-1]
+    tot_l = np.sum(ls)
     half_l = tot_l / 2
 
     # Get the half mass radius particle
@@ -157,7 +157,7 @@ def get_main(path, snap, savepath, filters, F, model, filename):
                                physicalUnits=True, numThreads=8)
         a_born = E.read_array('PARTDATA', path, snap, 'PartType4/StellarFormationTime', noH=True,
                               physicalUnits=True, numThreads=8)
-        metallicities = E.read_array('PARTDATA', path, snap, 'PartType4/SmoothedMetallicity', noH=True,
+        metallicities = E.read_array('PARTDATA', path, snap, 'PartType4/Metallicity', noH=True,
                                      physicalUnits=True, numThreads=8)
         masses = E.read_array('PARTDATA', path, snap, 'PartType4/InitialMass', noH=True, physicalUnits=True,
                               numThreads=8) * 10 ** 10
@@ -256,7 +256,7 @@ def get_main(path, snap, savepath, filters, F, model, filename):
     # Get gas particle information
     gas_all_poss = E.read_array('PARTDATA', path, snap, 'PartType0/Coordinates', noH=True, physicalUnits=True,
                                 numThreads=8)
-    gas_metallicities = E.read_array('PARTDATA', path, snap, 'PartType0/SmoothedMetallicity', noH=True,
+    gas_metallicities = E.read_array('PARTDATA', path, snap, 'PartType0/Metallicity', noH=True,
                                      physicalUnits=True, numThreads=8)
     gas_smooth_ls = E.read_array('PARTDATA', path, snap, 'PartType0/SmoothingLength', noH=True, physicalUnits=True,
                                  numThreads=8)
@@ -365,8 +365,8 @@ def get_main(path, snap, savepath, filters, F, model, filename):
                     centd_gas_pos = all_gas_poss[id] - means[id]
                     star_rs = calc_rad(centd_star_pos, i, j)
                     gas_rs = calc_rad(centd_gas_pos, i, j)
-                    okinds_star = star_rs <= 0.1
-                    okinds_gas = gas_rs <= 0.1
+                    okinds_star = star_rs <= 0.03
+                    okinds_gas = gas_rs <= 0.03
                     centd_star_pos = centd_star_pos[okinds_star]
                     centd_gas_pos = centd_gas_pos[okinds_gas]
                     ls = get_lumins(centd_star_pos, gal_ms[id][okinds_star], gal_ages[id][okinds_star],
