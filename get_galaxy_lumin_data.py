d696217993ae416b0424e8e003c743727bc3bf46
Will Roper <wjr21@sussex.ac.uk>
2020-07-01 15:14:49 +0100

    prog analysis

--- get_galaxy_lumin_data.py
+++ get_galaxy_lumin_data.py
@@ -95,7 +95,7 @@ def get_img_hlr(img, apertures, tot_l, app_rs):
     print(phot_table)
 
     # Extract the aperture luminosities
-    lumins = np.array(phot_table)[0][3:]
+    lumins = np.array(phot_table)[0]
 
     print(lumins)
 
