4281daf4f55233334145fc71906741b7eb1f54f9
Will Roper <wjr21@sussex.ac.uk>
2020-07-17 11:24:42 +0100

    passive animation

--- passive_animation_gassfr.py
+++ passive_animation_gassfr.py
@@ -72,7 +72,7 @@ def get_part_inds(halo_ids, part_ids, group_part_ids, sorted):
 
 
 regions = []
-for reg in range(0, 1):
+for reg in range(1, 5):
 
     if reg < 10:
         regions.append('0' + str(reg))
