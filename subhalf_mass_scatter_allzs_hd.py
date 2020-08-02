ae9382de1b3dcf589e968f01531db04b3e8a216c
Will Roper <wjr21@sussex.ac.uk>
2020-08-02 15:12:07 +0100

    high res size

--- subhalf_mass_scatter_allzs_hd.py
+++ subhalf_mass_scatter_allzs_hd.py
@@ -19,9 +19,9 @@ regions = []
 for reg in range(0, 40):
 
     if reg < 10:
-        regions.append('000' + str(reg))
+        regions.append('0' + str(reg))
     else:
-        regions.append('00' + str(reg))
+        regions.append(str(reg))
 
 snaps = ['003_z012p000', '004_z011p000', '005_z010p000',
          '006_z009p000', '007_z008p000', '008_z007p000',
