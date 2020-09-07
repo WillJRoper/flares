f7de97c350f2b2eb321ed84f57d55b07dad07fba
wjr21 <wjr21@sussex.ac.uk>
2020-09-07 19:14:34 +0100

    galaxy matching test

--- galaxy_matching.py
+++ galaxy_matching.py
@@ -79,15 +79,15 @@ res_ms_1 = []
 res_ms_2 = []
 for ind, cop in enumerate(cops1):
 
-    # ===================== Matching on phase =====================
-
-    # Find the 5 nearest neighbours
-    ds, inds = ptree.query(phases1[ind], k=1)
-
-    res_hmr_2.append(hmrs2[inds])
-    res_hmr_1.append(hmrs1[ind])
-    res_ms_2.append(ms2[inds])
-    res_ms_1.append(ms1[ind])
+    # # ===================== Matching on phase =====================
+    #
+    # # Find the 5 nearest neighbours
+    # ds, inds = ptree.query(phases1[ind], k=1)
+    #
+    # res_hmr_2.append(hmrs2[inds])
+    # res_hmr_1.append(hmrs1[ind])
+    # res_ms_2.append(ms2[inds])
+    # res_ms_1.append(ms1[ind])
 
     # # ===================== Matching on phase and total mass =====================
     #
@@ -147,15 +147,15 @@ for ind, cop in enumerate(cops1):
     # res_ms_2.append(ms2[inds[nn_ind]])
     # res_ms_1.append(ms1[ind])
 
-    # # ===================== Matching only on COP =====================
-    #
-    # # Find the 5 nearest neighbours
-    # ds, inds = tree.query(cop, k=1)
-    #
-    # res_hmr_2.append(hmrs2[inds])
-    # res_hmr_1.append(hmrs1[ind])
-    # res_ms_2.append(ms2[inds])
-    # res_ms_1.append(ms1[ind])
+    # ===================== Matching only on COP =====================
+
+    # Find the 5 nearest neighbours
+    ds, inds = tree.query(cop, k=1)
+
+    res_hmr_2.append(hmrs2[inds])
+    res_hmr_1.append(hmrs1[ind])
+    res_ms_2.append(ms2[inds])
+    res_ms_1.append(ms1[ind])
 
 res_hmr_2 = np.array(res_hmr_2)
 res_hmr_1 = np.array(res_hmr_1)
@@ -187,7 +187,7 @@ ax1.set_ylabel(r"$R_{1/2, hi} / [pkpc]$")
 ax2.set_xlabel(r"$M_{\star, std} / M_\odot$")
 ax2.set_ylabel(r"$M_{\star, hi} / M_\odot$")
 
-fig.savefig("plots/res_galaxy_match_phase.png", bbox_inches="tight")
+fig.savefig("plots/res_galaxy_match_cop_only.png", bbox_inches="tight")
 
 
 
