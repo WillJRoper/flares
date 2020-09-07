7905fd40eb478d0486ccc297dbe402e464a4cee0
wjr21 <wjr21@sussex.ac.uk>
2020-09-07 17:30:09 +0100

    galaxy matching test

--- galaxy_matching.py
+++ galaxy_matching.py
@@ -11,29 +11,33 @@ import seaborn as sns
 from scipy.spatial import cKDTree
 import pickle
 import itertools
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
-sns.set_style('whitegrid')
+sns.set_style("whitegrid")
 
 
-snap = '010_z005p000'
+snap = "010_z005p000"
 reg = "24"
 
-path1 = '/cosma7/data/dp004/FLARES/FLARES-HD/FLARES_HR_' + reg + '/data/'
-path2 = '/cosma7/data/dp004/FLARES/FLARES-1/G-EAGLE_' + reg + '/data/'
+path1 = "/cosma7/data/dp004/FLARES/FLARES-HD/FLARES_HR_" + reg + "/data/"
+path2 = "/cosma7/data/dp004/FLARES/FLARES-1/G-EAGLE_" + reg + "/data/"
+
+print("Matching on...")
+print(path1)
+print(path2)
 
 # Get the data for the standard res simulations
-hmrs1 = E.read_array('SUBFIND', path1, snap, 'Subhalo/HalfMassRad', noH=True, numThreads=8) * 1e3
-ms1 = E.read_array('SUBFIND', path1, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+hmrs1 = E.read_array("SUBFIND", path1, snap, "Subhalo/HalfMassRad", noH=True, numThreads=8) * 1e3
+ms1 = E.read_array("SUBFIND", path1, snap, "Subhalo/ApertureMeasurements/Mass/030kpc",
                   noH=True, numThreads=8)[:, 4] * 10**10
-cops1 = E.read_array('SUBFIND', path1, snap, 'Subhalo/CentreOfPotential', physicalUnits=True,
+cops1 = E.read_array("SUBFIND", path1, snap, "Subhalo/CentreOfPotential", physicalUnits=True,
                     noH=True, numThreads=8)
 
 # Get the data for the high resolution
-hmrs2 = E.read_array('SUBFIND', path2, snap, 'Subhalo/HalfMassRad', noH=True, numThreads=8) * 1e3
-ms2 = E.read_array('SUBFIND', path2, snap, 'Subhalo/ApertureMeasurements/Mass/030kpc',
+hmrs2 = E.read_array("SUBFIND", path2, snap, "Subhalo/HalfMassRad", noH=True, numThreads=8) * 1e3
+ms2 = E.read_array("SUBFIND", path2, snap, "Subhalo/ApertureMeasurements/Mass/030kpc",
                   noH=True, numThreads=8)[:, 4] * 10**10
-cops2 = E.read_array('SUBFIND', path2, snap, 'Subhalo/CentreOfPotential', physicalUnits=True,
+cops2 = E.read_array("SUBFIND", path2, snap, "Subhalo/CentreOfPotential", physicalUnits=True,
                     noH=True, numThreads=8)
 
 # Build the tree
@@ -59,10 +63,10 @@ fig = plt.figure()
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
 
-cbar = ax1.hexbin(res_hmr_1, res_hmr_2, gridsize=50, mincnt=1, xscale='log', yscale='log', norm=LogNorm(),
-                 linewidths=0.2, cmap='viridis', alpha=0.7)
-cbar = ax2.hexbin(res_ms_1, res_ms_2, gridsize=50, mincnt=1, xscale='log', yscale='log', norm=LogNorm(),
-                 linewidths=0.2, cmap='viridis', alpha=0.7)
+cbar = ax1.hexbin(res_hmr_1, res_hmr_2, gridsize=50, mincnt=1, xscale="log", yscale="log", norm=LogNorm(),
+                 linewidths=0.2, cmap="viridis", alpha=0.7)
+cbar = ax2.hexbin(res_ms_1, res_ms_2, gridsize=50, mincnt=1, xscale="log", yscale="log", norm=LogNorm(),
+                 linewidths=0.2, cmap="viridis", alpha=0.7)
 
 ax1.set_xlabel(r"$R_{1/2, std} / [pkpc]$")
 ax1.set_ylabel(r"$R_{1/2, hi} / [pkpc]$")
