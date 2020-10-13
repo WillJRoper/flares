924221c1a00139b347a4db34d28c6c684ac96547
wjr21 <wjr21@sussex.ac.uk>
2020-10-13 18:43:09 +0100

    evolution of birth density and metallicity

--- metallicity_birth_density_evolution.py
+++ metallicity_birth_density_evolution.py
@@ -160,7 +160,7 @@ def get_data(masslim=1e8, eagle=False):
         regions = ["EAGLE", ]
     else:
         regions = []
-        for reg in range(0, 40):
+        for reg in range(0, 1):
             if reg < 10:
                 regions.append('0' + str(reg))
             else:
