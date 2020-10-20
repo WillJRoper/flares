56213ca5166f8956587a6e945d0e4f874638b712
wjr21 <wjr21@sussex.ac.uk>
2020-10-20 13:02:17 +0100

    evolution of birth density and metallicity

--- metallicity_birth_density_evolution_od.py
+++ metallicity_birth_density_evolution_od.py
@@ -341,7 +341,7 @@ ax2.hexbin(zs_all[okinds], stellar_bd_all[okinds], gridsize=100, mincnt=1,
            yscale="log",  norm=LogNorm(),
            linewidths=0.2, cmap="Greys")
 
-ax2.text(0.8, 0.9, "$10^9 < M/M_\odot \leq 10^9.5$",
+ax2.text(0.8, 0.9, "$10^9 < M/M_\odot \leq 10^{9.5}$",
         bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
         transform=ax2.transAxes, horizontalalignment='right', fontsize=8)
 
@@ -351,7 +351,7 @@ ax3.hexbin(zs_all[okinds], stellar_bd_all[okinds], gridsize=100, mincnt=1,
            yscale="log",  norm=LogNorm(),
            linewidths=0.2, cmap="Greys")
 
-ax3.text(0.8, 0.9, "$10^9.5 < M/M_\odot \leq 10^10$",
+ax3.text(0.8, 0.9, "$10^{9.5} < M/M_\odot \leq 10^{10}$",
         bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
         transform=ax3.transAxes, horizontalalignment='right', fontsize=8)
 
@@ -361,7 +361,7 @@ ax4.hexbin(zs_all[okinds], stellar_bd_all[okinds], gridsize=100, mincnt=1,
            yscale="log",  norm=LogNorm(),
            linewidths=0.2, cmap="Greys")
 
-ax4.text(0.8, 0.9, "$10^10 < M/M_\odot$",
+ax4.text(0.8, 0.9, "$10^{10} < M/M_\odot$",
         bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
         transform=ax4.transAxes, horizontalalignment='right', fontsize=8)
 
@@ -391,6 +391,83 @@ ax4.tick_params(axis='y', left=False, right=False, labelleft=False,
                 labelright=False)
 
 fig.savefig("plots/stellarbd_z_evolution_mass.png", bbox_inches="tight")
+
+plt.close(fig)
+
+fig = plt.figure(figsize=(6, 6))
+gs = gridspec.GridSpec(nrows=2, ncols=2)
+gs.update(wspace=0.0, hspace=0.0)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[1, 0])
+ax3 = fig.add_subplot(gs[0, 1])
+ax4 = fig.add_subplot(gs[1, 1])
+
+okinds = np.logical_and(masses > 10**8, masses <= 10**9)
+
+ax1.hexbin(zs[okinds], stellar_bd[okinds], gridsize=100, mincnt=1,
+           yscale="log",  norm=LogNorm(),
+           linewidths=0.2, cmap="Greys")
+
+ax1.text(0.8, 0.9, "$10^8 < M/M_\odot \leq 10^9$",
+        bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
+        transform=ax1.transAxes, horizontalalignment='right', fontsize=8)
+
+okinds = np.logical_and(masses > 10**9, masses <= 10**9.5)
+
+ax2.hexbin(zs[okinds], stellar_bd[okinds], gridsize=100, mincnt=1,
+           yscale="log",  norm=LogNorm(),
+           linewidths=0.2, cmap="Greys")
+
+ax2.text(0.8, 0.9, "$10^9 < M/M_\odot \leq 10^{9.5}$",
+        bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
+        transform=ax2.transAxes, horizontalalignment='right', fontsize=8)
+
+okinds = np.logical_and(masses > 10**9.5, masses <= 10**10)
+
+ax3.hexbin(zs[okinds], stellar_bd[okinds], gridsize=100, mincnt=1,
+           yscale="log",  norm=LogNorm(),
+           linewidths=0.2, cmap="Greys")
+
+ax3.text(0.8, 0.9, "$10^{9.5} < M/M_\odot \leq 10^{10}$",
+        bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
+        transform=ax3.transAxes, horizontalalignment='right', fontsize=8)
+
+okinds = masses > 10**10
+
+ax4.hexbin(zs[okinds], stellar_bd[okinds], gridsize=100, mincnt=1,
+           yscale="log",  norm=LogNorm(),
+           linewidths=0.2, cmap="Greys")
+
+ax4.text(0.8, 0.9, "$10^{10} < M/M_\odot$",
+        bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1, alpha=0.8),
+        transform=ax4.transAxes, horizontalalignment='right', fontsize=8)
+
+ax2.set_xlabel(r"$z$")
+ax4.set_xlabel(r"$z$")
+ax1.set_ylabel(r"$\rho_{\mathrm{birth}}$ / [cm$^{-3}$]")
+ax2.set_ylabel(r"$\rho_{\mathrm{birth}}$ / [cm$^{-3}$]")
+
+xlims = []
+ylims = []
+for ax in [ax1, ax2, ax3, ax4]:
+    xlims.extend(ax.get_xlim())
+    ylims.extend(ax.get_ylim())
+
+for ax in [ax1, ax2, ax3, ax4]:
+    ax.set_xlim(0, 30)
+    ax.set_ylim(np.min(ylims) - 0.1 * np.min(ylims),
+                np.max(ylims) + 0.1 * np.max(ylims))
+
+# Remove axis labels
+ax1.tick_params(axis='x', top=False, bottom=False, labeltop=False,
+                labelbottom=False)
+ax3.tick_params(axis='both', left=False, top=False, right=False, bottom=False,
+                labelleft=False, labeltop=False,
+                labelright=False, labelbottom=False)
+ax4.tick_params(axis='y', left=False, right=False, labelleft=False,
+                labelright=False)
+
+fig.savefig("plots/stellarbd_z_evolution_mass_FLARES.png", bbox_inches="tight")
 
 plt.close(fig)
 
