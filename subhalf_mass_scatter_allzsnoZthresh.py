bb2c6f510b1032beb38c49b4d49a0616b34eaf51
wjr21 <wjr21@sussex.ac.uk>
2020-11-02 01:33:26 +0000

    evolution of metallicity gradients

--- subhalf_mass_scatter_allzsnoZthresh.py
+++ subhalf_mass_scatter_allzsnoZthresh.py
@@ -18,8 +18,7 @@ sns.set_style('whitegrid')
 snaps = ['003_z012p000', '004_z011p000', '005_z010p000',
          '006_z009p000', '007_z008p000', '008_z007p000',
          '009_z006p000', '010_z005p000', '011_z004p770']
-path = "/cosma7/data/Eagle/ScienceRuns/Planck1/L0025N0376/" \
-                       "PE/EagleVariation_NoZDEPSFthresh/data"
+path = "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/FLARES_00_noZSFthresh/data"
 path2 = '/cosma7/data/dp004/dc-love2/data/G-EAGLE/geagle_0000/data/'
 axlims_x = []
 axlims_y = []
